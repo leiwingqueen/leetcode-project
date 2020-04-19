a8fb3ba645f089e4656b1096eef33c01755474ad
yongquanli <yongquanli@kugou.net>
2020-04-19 09:38:43 +0800

    最大礼物的DP解法

--- src/main/java/com/liyongquan/dp/Gift2.java
+++ src/main/java/com/liyongquan/dp/Gift2.java
@@ -1,8 +1,5 @@
 package com.liyongquan.dp;
 
-import java.util.LinkedList;
-import java.util.Queue;
-
 /**
  * 在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。
  * 你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
@@ -31,15 +28,16 @@ import java.util.Queue;
  * 链接：https://leetcode-cn.com/problems/li-wu-de-zui-da-jie-zhi-lcof
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
-public class Gift {
+public class Gift2 {
     /**
+     * 这次采用dp的方式解决
      * dp方程：f(i,j)=max{f(i-1,j),f(i,j-1)}+S[i][j]
      * 初始化：
      * i=0,j=0. f[0][0]=S[0][0]
      *
-     * 遍历的过程可以使用BFS来实现,算法复杂度为O(m*n)
+     * if i=0,f[0][j]=f[0][j-1]+S[0][j]
+     * if j=0,f[i][0]=f[i-1][0]+S[i][0]
      *
-     * BFS+减枝
      * @param grid
      * @return
      */
@@ -47,32 +45,17 @@ public class Gift {
         int row=grid.length;
         int col=grid[0].length;
         int[][] result=new int[row][col];
-        Queue<int[]> queue=new LinkedList<>();
-        queue.add(new int[]{0,0});
-        while (!queue.isEmpty()){
-            int[] poll = queue.poll();
-            int x = poll[0];
-            int y = poll[1];
-            int r=0;
-            if (x==0&&y==0) {
-                r=grid[0][0];
-            }else if(x==0) {
-                r=result[x][y-1]+grid[x][y];
-            }else if(y==0) {
-                r=result[x-1][y]+grid[x][y];
-            }else{
-                r=Math.max(result[x-1][y],result[x][y-1])+grid[x][y];
-            }
-            result[x][y]=r;
-            //已经计算的点就不再计算
-            if(x < row-1&&result[x+1][y]==0){
-                queue.add(new int[]{x+1,y});
-                //标记为已经进队列，不再重复计算
-                result[x+1][y]=-1;
-            }
-            if(y< col-1&&result[x][y+1]==0){
-                queue.add(new int[]{x,y+1});
-                result[x][y+1]=-1;
+        result[0][0]=grid[0][0];
+        //第一行和第一列初始化
+        for (int i = 1; i < col; i++) {
+            result[0][i]=result[0][i-1]+grid[0][i];
+        }
+        for (int i = 1; i < row; i++) {
+            result[i][0]=result[i-1][0]+grid[i][0];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                result[i][j]=Math.max(result[i-1][j],result[i][j-1])+grid[i][j];
             }
         }
         return result[row-1][col-1];
