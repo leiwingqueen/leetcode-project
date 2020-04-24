275c11c495e59d2d3f18db7a57a6098d655c55d7
yongquanli <yongquanli@kugou.net>
2020-04-24 20:19:07 +0800

    排序算法

---
+++ src/main/java/com/liyongquan/array/LeastNumbers.java
@@ -0,0 +1,45 @@
+package com.liyongquan.array;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * 输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：arr = [3,2,1], k = 2
+ * 输出：[1,2] 或者 [2,1]
+ *
+ * 示例 2：
+ *
+ * 输入：arr = [0,1,2,1], k = 1
+ * 输出：[0]
+ *
+ *
+ *
+ * 限制：
+ *
+ *     0 <= k <= arr.length <= 10000
+ *     0 <= arr[i] <= 10000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LeastNumbers {
+    public int[] getLeastNumbers(int[] arr, int k) {
+        List<Integer> list=new ArrayList<>();
+        for (int i : arr) {
+            list.add(i);
+        }
+        Collections.sort(list);
+        int[] r=new int[k];
+        for (int i = 0; i < k; i++) {
+            r[i]=list.get(i);
+        }
+        return r;
+    }
+}
