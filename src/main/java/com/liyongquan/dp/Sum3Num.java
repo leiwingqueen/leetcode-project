def289a13a13c148553eec605884bd0eec3b7ece
liyongquan <496326100@qq.com>
2020-06-01 22:37:11 +0800

    三元组的和

--- src/main/java/com/liyongquan/dp/Sum3Num.java
+++ src/main/java/com/liyongquan/dp/Sum3Num.java
@@ -1,9 +1,6 @@
 package com.liyongquan.dp;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
@@ -27,15 +24,48 @@ import java.util.List;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Sum3Num {
+    /**
+     * 特判，对于数组长度 nn，如果数组为 nullnull 或者数组长度小于 33，返回 [][]。
+     * 对数组进行排序。
+     * 遍历排序后数组：
+     * 若 nums[i]>0nums[i]>0：因为已经排序好，所以后面不可能有三个数加和等于 00，直接返回结果。
+     * 对于重复元素：跳过，避免出现重复解
+     * 令左指针 L=i+1L=i+1，右指针 R=n-1R=n−1，当 L<RL<R 时，执行循环：
+     * 当 nums[i]+nums[L]+nums[R]==0nums[i]+nums[L]+nums[R]==0，执行循环，判断左界和右界是否和下一位置重复，去除重复解。并同时将 L,RL,R 移到下一位置，寻找新的解
+     * 若和大于 00，说明 nums[R]nums[R] 太大，RR 左移
+     * 若和小于 00，说明 nums[L]nums[L] 太小，LL 右移
+     * <p>
+     * 作者：wu_yan_zu
+     * 链接：https://leetcode-cn.com/problems/3sum/solution/pai-xu-shuang-zhi-zhen-zhu-xing-jie-shi-python3-by/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * @param nums
+     * @return
+     */
     public List<List<Integer>> threeSum(int[] nums) {
+        //sort
         int[] ints = Arrays.stream(nums).sorted().toArray();
+        Set<String> set = new HashSet<>();
         List<List<Integer>> result = new ArrayList<>();
         for (int i = 0; i < ints.length - 2; i++) {
-            for (int j = i + 1; j < ints.length - 1; j++) {
-                for (int k = j+1; k < ints.length; k++) {
-                    if (ints[i] + ints[j] + ints[k] == 0) {
-                        result.add(Arrays.asList(ints[i], ints[j], ints[k]));
+            if (ints[i] > 0) {
+                break;
+            }
+            int left = i + 1, right = ints.length-1;
+            while (left < right) {
+                int sum = ints[i] + ints[left] + ints[right];
+                if (sum == 0) {
+                    if (!set.contains(ints[i] + "_" + ints[left] + "_" + ints[right])) {
+                        result.add(Arrays.asList(ints[i], ints[left], ints[right]));
+                        set.add(ints[i] + "_" + ints[left] + "_" + ints[right]);
                     }
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
                 }
             }
         }
