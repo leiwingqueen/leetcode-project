e68d76f251300e1b1ae5986e0ed07ac789329a40
liyongquan <496326100@qq.com>
2020-06-13 23:26:16 +0800

    爬楼梯

--- src/main/java/com/liyongquan/dp/Stair.java
+++ src/main/java/com/liyongquan/dp/Stair.java
@@ -1,25 +1,31 @@
 package com.liyongquan.dp;
 
 public class Stair {
+    /**
+     * dp表达式
+     * f(n)=f(n-1)+f(n-2)
+     * 初始化：
+     * f(1)=1
+     * f(2)=2
+     *
+     * @param n
+     * @return
+     */
     public int climbStairs(int n) {
-        if (n==1) {
-            return 1;
+        if (n <= 2) {
+            return n;
         }
-        if (n==2) {
-            return 2;
-        }
-        int dp0=1;
-        int dp1=2;
+        int dp0 = 1, dp1 = 2;
         for (int i = 3; i <= n; i++) {
-            int temp=dp1;
-            dp1=dp0+dp1;
-            dp0=temp;
+            int temp = dp1;
+            dp1 = dp0 + dp1;
+            dp0 = temp;
         }
         return dp1;
     }
 
     public static void main(String[] args) {
-        Stair stair=new Stair();
+        Stair stair = new Stair();
         System.out.println(stair.climbStairs(3));
     }
 }
