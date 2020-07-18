7ef701110fda55338011d04b20443e092ecf8231
liyongquan <496326100@qq.com>
2020-07-18 10:44:43 +0800

    树的数量

--- src/main/java/com/liyongquan/binarySort/NumTrees.java
+++ src/main/java/com/liyongquan/binarySort/NumTrees.java
@@ -1,5 +1,7 @@
 package com.liyongquan.binarySort;
 
+import java.util.concurrent.ForkJoinPool;
+
 /**
  * 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
  * <p>
@@ -21,6 +23,47 @@ package com.liyongquan.binarySort;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class NumTrees {
+    /**
+     * 正常的递归解法会超时，这里尝试似乎dp的方式解题
+     * f(m,n)=sum(f(m,i-1)*f(i+1,n)),其中m<=i<=n
+     * 时间复杂度O(n^3)
+     *
+     * @param n
+     * @return
+     */
+    public int numTrees2(int n) {
+        int[][] result = new int[n + 2][n + 2];
+        //对角线、横线、斜线赋值
+        for (int i = 0; i < n + 2; i++) {
+            result[i][i] = 1;
+            result[0][i] = 1;
+            result[i][0] = 1;
+            result[i][n + 1] = 1;
+            result[n + 1][i] = 1;
+        }
+        //下半区域赋值
+        for (int i = 0; i < n + 2; i++) {
+            for (int j = 0; j <= i; j++) {
+                result[i][j] = 1;
+            }
+        }
+        int col = 1;
+        while (col <= n) {
+            int i = 1;
+            while (col + i - 1 <= n) {
+                //计算result[i][col+i-1]的值
+                int sum = 0;
+                for (int j = i; j <= col + i - 1; j++) {
+                    sum += result[i][j - 1] * result[j + 1][col + i - 1];
+                }
+                result[i][col + i - 1] = sum;
+                i++;
+            }
+            col++;
+        }
+        return result[1][n];
+    }
+
     public int numTrees(int n) {
         return degrace(1, n);
     }
@@ -49,7 +92,7 @@ public class NumTrees {
 
     public static void main(String[] args) {
         NumTrees trees = new NumTrees();
-        int i = trees.numTrees(3);
+        int i = trees.numTrees2(3);
         System.out.println(i);
     }
 }
