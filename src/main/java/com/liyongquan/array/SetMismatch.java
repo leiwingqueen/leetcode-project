64080a42481b7802aedcce9dd6a82fdca8e32029
liyongquan <496326100@qq.com>
2020-08-09 17:54:58 +0800

    错误集合

--- src/main/java/com/liyongquan/array/SetMismatch.java
+++ src/main/java/com/liyongquan/array/SetMismatch.java
@@ -1,8 +1,5 @@
 package com.liyongquan.array;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * 集合 S 包含从1到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个元素复制了成了集合里面的另外一个元素的值，导致集合丢失了一个整数并且有一个元素重复。
  * <p>
