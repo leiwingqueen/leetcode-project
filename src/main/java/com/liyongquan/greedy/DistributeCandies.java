8423ac20452722a12a9873a82f15e16e0358187f
liyongquan <496326100@qq.com>
2020-08-30 11:24:41 +0800

    分糖果-解法2

--- src/main/java/com/liyongquan/greedy/DistributeCandies.java
+++ src/main/java/com/liyongquan/greedy/DistributeCandies.java
@@ -1,5 +1,8 @@
 package com.liyongquan.greedy;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * 给定一个偶数长度的数组，其中不同的数字代表着不同种类的糖果，每一个数字代表一个糖果。你需要把这些糖果平均分给一个弟弟和一个妹妹。返回妹妹可以获得的最大糖果的种类数。
  * <p>
@@ -45,6 +48,20 @@ public class DistributeCandies {
             i++;
         }
         return count;
+    }
+
+    /**
+     * 解法2，改为set
+     *
+     * @param candies
+     * @return
+     */
+    public int distributeCandies2(int[] candies) {
+        Set<Integer> set = new HashSet<>(candies.length);
+        for (int i = 0; i < candies.length; i++) {
+            set.add(candies[i]);
+        }
+        return Math.min(set.size(), candies.length / 2);
     }
 
     public static void main(String[] args) {
