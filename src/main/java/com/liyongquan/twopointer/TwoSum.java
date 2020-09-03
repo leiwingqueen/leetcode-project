f51ec1846e6367e40146348a48e08108fc5dc905
liyongquan <496326100@qq.com>
2020-09-03 17:51:04 +0800

    两数之和-暴力解法

---
+++ src/main/java/com/liyongquan/twopointer/TwoSum.java
@@ -0,0 +1,43 @@
+package com.liyongquan.twopointer;
+
+import javafx.beans.binding.NumberBinding;
+
+/**
+ * 给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+ * <p>
+ * 函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+ * <p>
+ * 说明:
+ * <p>
+ * 返回的下标值（index1 和 index2）不是从零开始的。
+ * 你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+ * 示例:
+ * <p>
+ * 输入: numbers = [2, 7, 11, 15], target = 9
+ * 输出: [1,2]
+ * 解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum-ii-input-array-is-sorted
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TwoSum {
+    /**
+     * 暴力解法
+     * 没有利用递增的特性，时间复杂度O(n*(n-1)/2)
+     *
+     * @param numbers
+     * @param target
+     * @return
+     */
+    public int[] twoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[]{i+1, j+1};
+                }
+            }
+        }
+        return new int[]{};
+    }
+}
