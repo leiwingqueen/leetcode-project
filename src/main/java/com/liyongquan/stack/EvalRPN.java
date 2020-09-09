98fd553b23b38d13a7ec5e5171bb59c3a9a105c9
liyongquan <496326100@qq.com>
2020-09-09 11:43:16 +0800

    使用数组模拟栈

--- src/main/java/com/liyongquan/stack/EvalRPN.java
+++ src/main/java/com/liyongquan/stack/EvalRPN.java
@@ -105,6 +105,51 @@ public class EvalRPN {
         return -1;
     }
 
+    /********************下面这个解法太优秀***************************/
+    /**
+     *  数组最大长度为tokens.length / 2 + 1
+     * switch代替if-else，效率优化
+     * Integer.parseInt代替Integer.valueOf,减少自动拆箱装箱操作
+     *
+     * 作者：hteason
+     * 链接：https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/solution/javachun-shu-zu-he-zhan-liang-chong-shi-xian-jian-/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *   作者：hteason
+     *     链接：https://leetcode-cn.com/problems/evaluate-reverse-polish-notation/solution/javachun-shu-zu-he-zhan-liang-chong-shi-xian-jian-/
+     *     来源：力扣（LeetCode）
+     *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param tokens
+     * @return
+     */
+    //纯数组模拟栈实现(推荐)   3 ms	36 MB
+    public static int evalRPN2(String[] tokens) {
+        int[] numStack = new int[tokens.length / 2 + 1];
+        int index = 0;
+        for (String s : tokens) {
+            switch (s) {
+                case "+":
+                    numStack[index - 2] += numStack[--index];
+                    break;
+                case "-":
+                    numStack[index - 2] -= numStack[--index];
+                    break;
+                case "*":
+                    numStack[index - 2] *= numStack[--index];
+                    break;
+                case "/":
+                    numStack[index - 2] /= numStack[--index];
+                    break;
+                default:
+                    // numStack[index++] = Integer.valueOf(s);
+                    //valueOf改为parseInt，减少自动拆箱装箱操作
+                    numStack[index++] = Integer.parseInt(s);
+                    break;
+            }
+        }
+        return numStack[0];
+    }
+
     public static void main(String[] args) {
         EvalRPN rpn = new EvalRPN();
         int i = rpn.evalRPN(new String[]{"4", "13", "5", "/", "+"});
