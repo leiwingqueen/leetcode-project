709cfafdd1622ad91811c28b9c8a0e63efaedd0d
liyongquan <496326100@qq.com>
2020-09-14 11:01:13 +0800

    零钱兑换

--- src/main/java/com/liyongquan/dp/CoinChange.java
+++ src/main/java/com/liyongquan/dp/CoinChange.java
@@ -1,10 +1,5 @@
 package com.liyongquan.dp;
 
-import jdk.nashorn.internal.ir.IfNode;
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * 给定不同面额的硬币 coins 和一个总金额 amount。编写一个函数来计算可以凑成总金额所需的最少的硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
@@ -72,6 +67,7 @@ public class CoinChange {
             if (index == coins[i]) {
                 return 1;
             }
+            //子问题无解
             if (index < coins[i]) {
                 continue;
             }
