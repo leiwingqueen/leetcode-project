2512a56a49ce71946c5eb65fe0cd0464e5454f67
liyongquan <496326100@qq.com>
2020-09-15 23:37:28 +0800

    发票匹配问题

--- src/main/java/com/liyongquan/dp/Ticket.java
+++ src/main/java/com/liyongquan/dp/Ticket.java
@@ -9,6 +9,8 @@ public class Ticket {
      * <p>
      * dp方程：
      * f(k,w)=min{f(k-1,w),f(k-1,w-s[k])+s[k]}
+     * <p>
+     * f(k,w)为前k个发票能够组成满足w金额的最小发票组合。s[k]为第k张发票的金额
      *
      * @param s 发票金额
      * @param w 台账金额
@@ -21,7 +23,7 @@ public class Ticket {
             if (s[0] >= i) {
                 dp[0][i] = s[0];
             } else {
-                dp[0][i] = Integer.MAX_VALUE;
+                dp[0][i] = -1;
             }
         }
         for (int i = 0; i < s.length; i++) {
@@ -31,9 +33,18 @@ public class Ticket {
         for (int i = 1; i < s.length; i++) {
             for (int j = 1; j <= w; j++) {
                 if (j >= s[i]) {
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i - 1][j - s[i]] + s[i]);
-                } else {
+                    //不能直接取min,-1的场景要特殊处理
                     dp[i][j] = dp[i - 1][j];
+                    int temp = dp[i - 1][j - s[i]];
+                    if (temp > 0 && (dp[i][j] < 0 || temp + s[i] < dp[i][j])) {
+                        dp[i][j] = temp + s[i];
+                    }
+                } else {
+                    //j<s[i]，这意味着如果选择s[i]的话，s[i]就是最优解了
+                    dp[i][j] = dp[i - 1][j];
+                    if (dp[i][j] < 0 || s[i] < dp[i][j]) {
+                        dp[i][j] = s[i];
+                    }
                 }
             }
         }
--- src/test/java/com/liyongquan/dp/TicketTest.java
+++ src/test/java/com/liyongquan/dp/TicketTest.java
@@ -1,13 +1,28 @@
 package com.liyongquan.dp;
 
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class TicketTest {
     @Test
-    public void match(){
-        Ticket ticket=new Ticket();
-        int match = ticket.match(new int[]{100, 200, 500, 350}, 400);
-        System.out.println(match);
+    public void match() {
+        //刚好满足
+        Ticket ticket = new Ticket();
+        int match = ticket.match(new int[]{1, 2, 5, 3}, 4);
+        Assert.assertEquals(4, match);
+        match = ticket.match(new int[]{1, 2, 5, 3}, 9);
+        Assert.assertEquals(9, match);
+        //不足的场景
+        match = ticket.match(new int[]{1, 2, 5, 3}, 13);
+        Assert.assertEquals(-1, match);
+    }
+
+    @Test
+    public void match_over() {
+        //有多余的场景
+        Ticket ticket = new Ticket();
+        int match = ticket.match(new int[]{1, 4, 5, 9}, 7);
+        Assert.assertEquals(9, match);
     }
 }
