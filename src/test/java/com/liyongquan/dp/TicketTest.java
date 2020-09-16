bfc2fb74f097571b5c4c59e0f10bc7d04653d74b
liyongquan <496326100@qq.com>
2020-09-16 11:25:42 +0800

    发票金额修改

--- src/test/java/com/liyongquan/dp/TicketTest.java
+++ src/test/java/com/liyongquan/dp/TicketTest.java
@@ -9,20 +9,44 @@ public class TicketTest {
     public void match() {
         //刚好满足
         Ticket ticket = new Ticket();
-        int match = ticket.match(new int[]{1, 2, 5, 3}, 4);
-        Assert.assertEquals(4, match);
+        Ticket.TicketPair match = ticket.match(new int[]{1, 2, 5, 3}, 4);
+        System.out.println("发票的金额总和:" + match.weight);
+        StringBuilder builder = new StringBuilder();
+        for (Integer i : match.list) {
+            builder.append(i + ",");
+        }
+        System.out.println("选择的发票序号:" + builder);
+        Assert.assertEquals(4, match.weight);
         match = ticket.match(new int[]{1, 2, 5, 3}, 9);
-        Assert.assertEquals(9, match);
+        System.out.println("发票的金额总和:" + match.weight);
+        builder = new StringBuilder();
+        for (Integer i : match.list) {
+            builder.append(i + ",");
+        }
+        System.out.println("选择的发票序号:" + builder);
+        Assert.assertEquals(9, match.weight);
         //不足的场景
         match = ticket.match(new int[]{1, 2, 5, 3}, 13);
-        Assert.assertEquals(-1, match);
+        System.out.println("发票的金额总和:" + match.weight);
+        builder = new StringBuilder();
+        for (Integer i : match.list) {
+            builder.append(i + ",");
+        }
+        System.out.println("选择的发票序号:" + builder);
+        Assert.assertEquals(-1, match.weight);
     }
 
     @Test
     public void match_over() {
         //有多余的场景
         Ticket ticket = new Ticket();
-        int match = ticket.match(new int[]{1, 4, 5, 9}, 7);
-        Assert.assertEquals(9, match);
+        Ticket.TicketPair match = ticket.match(new int[]{1, 4, 5, 9}, 7);
+        System.out.println("发票的金额总和:" + match.weight);
+        StringBuilder builder = new StringBuilder();
+        for (Integer i : match.list) {
+            builder.append(i + ",");
+        }
+        System.out.println("选择的发票序号:" + builder);
+        Assert.assertEquals(9, match.weight);
     }
 }
