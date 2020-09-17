84f196c074871fb0d8ca19269a2b71373d50293a
liyongquan <496326100@qq.com>
2020-09-17 23:45:52 +0800

    除法-3种解法

--- src/main/java/com/liyongquan/math/Divide.java
+++ src/main/java/com/liyongquan/math/Divide.java
@@ -51,4 +51,84 @@ public class Divide {
         }
         return dividend / divisor;
     }
+
+    /**
+     * 能够勉强通过，但是效率比较糟糕，因此每一步移动太慢
+     * 解法2-不断减去除数，统计除数的数量
+     *
+     * @param dividend
+     * @param divisor
+     * @return
+     */
+    public int divide2(int dividend, int divisor) {
+        if (divisor == 1) {
+            return dividend;
+        }
+        if (divisor == -1) {
+            if (dividend == Integer.MIN_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            return -dividend;
+        }
+        int flag = (dividend ^ divisor) < 0 ? -1 : 1;
+        int result = 0;
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+        long tmp = b;
+        while (a >= b) {
+            result++;
+            b += tmp;
+        }
+        return flag > 0 ? result : -result;
+    }
+
+    /**
+     * 解法3，有点类似tcp拥堵阻塞算法，每次*2加速收敛的速度
+     *
+     * @param dividend
+     * @param divisor
+     * @return
+     */
+    public int divide3(int dividend, int divisor) {
+        //边界场景
+        if (divisor == 1) {
+            return dividend;
+        }
+        if (divisor == -1) {
+            if (dividend == Integer.MIN_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            return -dividend;
+        }
+        int flag = (dividend ^ divisor) < 0 ? -1 : 1;
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+        if (a < b) {
+            return 0;
+        }
+        //使用*2的运算加速收敛
+        long result = 1;
+        while (a >= b) {
+            result <<= 1;
+            b <<= 1;
+        }
+        //最终锁定范围在result/2~result之间的范围，再通过解法2求解
+        b >>= 1;
+        result >>= 1;
+        result -= 1;
+        long tmp = Math.abs((long) divisor);
+        while (a >= b) {
+            result++;
+            b += tmp;
+        }
+        return flag > 0 ? (int) result : -(int) result;
+    }
+
+    public static void main(String[] args) {
+        Divide divide = new Divide();
+        //int i = divide.divide3(7, -3);
+        //System.out.println(i);
+        int i = divide.divide3(2, 2);
+        System.out.println(i);
+    }
 }
