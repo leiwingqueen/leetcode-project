cee16cb0f9b2520fddee1a601e5d8c53e3092e96
liyongquan <496326100@qq.com>
2020-09-17 22:51:28 +0800

    除法-无视官方限制

---
+++ src/main/java/com/liyongquan/math/Divide.java
@@ -0,0 +1,54 @@
+package com.liyongquan.math;
+
+/**
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ * <p>
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ * <p>
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ * <p>
+ *  
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ * 解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+ * 示例 2:
+ * <p>
+ * 输入: dividend = 7, divisor = -3
+ * 输出: -2
+ * 解释: 7/-3 = truncate(-2.33333..) = -2
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 被除数和除数均为 32 位有符号整数。
+ * 除数不为 0。
+ * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [−231,  231 − 1]。本题中，如果除法结果溢出，则返回 231 − 1。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/divide-two-integers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Divide {
+    /**
+     * 无视官方限制，解题。需要考虑溢出的场景
+     *
+     * @param dividend
+     * @param divisor
+     * @return
+     */
+    public int divide(int dividend, int divisor) {
+        if (divisor == 1) {
+            return dividend;
+        }
+        if (divisor == -1) {
+            if (dividend == Integer.MIN_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+            return -dividend;
+        }
+        return dividend / divisor;
+    }
+}
