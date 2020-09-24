8d5fd8adde4ec23a8cc689d772d18d4ddfcceece
liyongquan <496326100@qq.com>
2020-09-24 13:05:50 +0800

    日常刷题

---
+++ src/main/java/com/liyongquan/dp/MinDistance2.java
@@ -0,0 +1,40 @@
+package com.liyongquan.dp;
+
+/**
+ * 给你两个单词 word1 和 word2，请你计算出将 word1 转换成 word2 所使用的最少操作数 。
+ * <p>
+ * 你可以对一个单词进行如下三种操作：
+ * <p>
+ * 插入一个字符
+ * 删除一个字符
+ * 替换一个字符
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：word1 = "horse", word2 = "ros"
+ * 输出：3
+ * 解释：
+ * horse -> rorse (将 'h' 替换为 'r')
+ * rorse -> rose (删除 'r')
+ * rose -> ros (删除 'e')
+ * 示例 2：
+ * <p>
+ * 输入：word1 = "intention", word2 = "execution"
+ * 输出：5
+ * 解释：
+ * intention -> inention (删除 't')
+ * inention -> enention (将 'i' 替换为 'e')
+ * enention -> exention (将 'n' 替换为 'x')
+ * exention -> exection (将 'n' 替换为 'c')
+ * exection -> execution (插入 'u')
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/edit-distance
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MinDistance2 {
+    public int minDistance(String word1, String word2) {
+        return 0;
+    }
+}
