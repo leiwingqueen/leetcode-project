75fd3dc508f3e11e3ef09e04503d7ccc67aa219f
liyongquan <496326100@qq.com>
2020-09-25 15:29:43 +0800

    翻转字符串

---
+++ src/main/java/com/liyongquan/array/FlipedString.java
@@ -0,0 +1,54 @@
+package com.liyongquan.array;
+
+/**
+ * 字符串轮转。给定两个字符串s1和s2，请编写代码检查s2是否为s1旋转而成（比如，waterbottle是erbottlewat旋转后的字符串）。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入：s1 = "waterbottle", s2 = "erbottlewat"
+ * 输出：True
+ * 示例2:
+ * <p>
+ * 输入：s1 = "aa", s2 = "aba"
+ * 输出：False
+ * 提示：
+ * <p>
+ * 字符串长度在[0, 100000]范围内。
+ * 说明:
+ * <p>
+ * 你能只调用一次检查子串的方法吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/string-rotation-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FlipedString {
+    /**
+     * 傻瓜解法
+     *
+     * @param s1
+     * @param s2
+     * @return
+     */
+    public boolean isFlipedString(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        for (int i = 0; i < s1.length(); i++) {
+            if (eq(s1, s2, i)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean eq(String s1, String s2, int start) {
+        for (int i = 0; i < s1.length(); i++) {
+            int j = (start + i) % s1.length();
+            if (s1.charAt(i) != s2.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
