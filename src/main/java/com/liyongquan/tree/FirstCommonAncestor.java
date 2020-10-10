3bd307b409d7e1b527dc2494c9d9919ea228854d
liyongquan <496326100@qq.com>
2020-10-10 14:50:04 +0800

    公共祖先-解法3

--- src/main/java/com/liyongquan/tree/FirstCommonAncestor.java
+++ src/main/java/com/liyongquan/tree/FirstCommonAncestor.java
@@ -141,4 +141,26 @@ public class FirstCommonAncestor {
         result.pollLast();
         return false;
     }
+
+    /**
+     * 作者：yinkn
+     *     链接：https://leetcode-cn.com/problems/first-common-ancestor-lcci/solution/jian-ji-de-xie-fa-by-yinkn/
+     *     来源：力扣（LeetCode）
+     *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * 还有一种递归写法，这个是真的没想到。。。
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor3(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (root == p || root == q) return root;
+        else if (left != null && right != null) return root;
+        else if (left != null) return left;
+        else return right;
+    }
 }
