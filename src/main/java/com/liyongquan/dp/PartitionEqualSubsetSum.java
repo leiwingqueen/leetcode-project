8a8ad0ead2bbbd7ec6dfa3ef74b81522040c3c5e
liyongquan <496326100@qq.com>
2020-10-11 18:41:57 +0800

    分成两个数组-dp解法

--- src/main/java/com/liyongquan/dp/PartitionEqualSubsetSum.java
+++ src/main/java/com/liyongquan/dp/PartitionEqualSubsetSum.java
@@ -33,6 +33,8 @@ public class PartitionEqualSubsetSum {
      * 不做任何剪枝,时间复杂度O(2^n)，空间复杂度O(1)
      * <p>
      * 不能通过，超时~~~
+     * <p>
+     * 剪枝还是过不了
      *
      * @param nums
      * @return
@@ -52,10 +54,68 @@ public class PartitionEqualSubsetSum {
         if (index >= nums.length) {
             return left == right;
         }
-        boolean l = dfs(nums, index + 1, left + nums[index], right - nums[index]);
-        if (l) {
+        //适当剪枝
+        if (left < right) {
+            boolean l = dfs(nums, index + 1, left + nums[index], right - nums[index]);
+            if (l) {
+                return true;
+            }
+            return dfs(nums, index + 1, left, right);
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * 既然暴力+剪枝没法过，我们换个思路。
+     * <p>
+     * 分成等分的两部分，相当于能够在数组中找到一组数组=sum/2，sum为所有数字的总和(显然如果sum为奇数的话是可以直接返回false)
+     * <p>
+     * 我们假设f(n,s)表示前n个数字中，是否能够组成合为s的方程。这里我们就可以发现，这变成一个典型的背包问题了。
+     * <p>
+     * dp方程:
+     * f(n,s)=f(n-1,s) || f(n-1,s-nums[n-1])
+     * <p>
+     * 初始化：
+     * 直接把n=1的场景初始化。
+     * <p>
+     * 时间复杂度O(n*s)，空间复杂度O(n*s)，n为节点个数，s为nums的总和/2
+     *
+     * @param nums
+     * @return
+     */
+    public boolean canPartition2(int[] nums) {
+        if (nums.length == 0) {
             return true;
         }
-        return dfs(nums, index + 1, left, right);
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        //奇偶判断
+        if ((sum & 0b1) != 0) {
+            return false;
+        }
+        sum >>= 1;
+        boolean[][] dp = new boolean[nums.length][sum + 1];
+        //初始化
+        dp[0][0] = true;
+        for (int i = 1; i <= sum; i++) {
+            dp[0][i] = nums[0] == i;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][0] = true;
+        }
+        //dp迭代
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (nums[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i]];
+                }
+            }
+        }
+        return dp[nums.length - 1][sum];
     }
 }
