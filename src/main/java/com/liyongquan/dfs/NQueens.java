3ec02cb7799802cc31bd7b8874a03daa52ac7782
liyongquan <496326100@qq.com>
2020-10-21 11:46:04 +0800

    n皇后问题-优化

--- src/main/java/com/liyongquan/dfs/NQueens.java
+++ src/main/java/com/liyongquan/dfs/NQueens.java
@@ -1,6 +1,5 @@
 package com.liyongquan.dfs;
 
-import javax.management.relation.RelationSupport;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -46,6 +45,7 @@ import java.util.List;
 public class NQueens {
     /**
      * 回溯
+     *
      * @param n
      * @return
      */
@@ -96,6 +96,15 @@ public class NQueens {
         return result;
     }
 
+    /**
+     * 这里的判断时间复杂度是O(n)，我们可以尝试把它简化成O(1)
+     *
+     * @param grid
+     * @param n
+     * @param x
+     * @param y
+     * @return
+     */
     private boolean canSet(int[][] grid, int n, int x, int y) {
         //正上方
         for (int i = 0; i <= x - 1; i++) {
@@ -119,5 +128,97 @@ public class NQueens {
             }
         }
         return true;
+    }
+
+
+    /**
+     * 回溯解法2
+     * 1.记录前面的移动我们简化成使用1维数据
+     * 2.判断是否能放置皇后的逻辑我们简化成O(1)的复杂度
+     *
+     * @param n
+     * @return
+     */
+    public List<List<String>> solveNQueens2(int n) {
+        int[] queens = new int[n], col = new int[n], dia1 = new int[2 * n - 1], dia2 = new int[2 * n - 1];
+        return dfs2(queens, n, 0, col, dia1, dia2);
+    }
+
+
+    private List<List<String>> dfs2(int[] queens, int n, int row, int[] col, int[] dia1, int[] dia2) {
+        if (row == n - 1) {
+            List<List<String>> result = new LinkedList<>();
+            for (int i = 0; i < n; i++) {
+                if (canSet2(n, row, i, col, dia1, dia2)) {
+                    queens[row] = i;
+                    result.add(makeStr2(queens, n));
+                    //回溯
+                    queens[row] = 0;
+                }
+            }
+            return result;
+        }
+        List<List<String>> result = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (canSet2(n, row, i, col, dia1, dia2)) {
+                queens[row] = i;
+                col[i] = 1;
+                dia1[row - i + n - 1] = 1;
+                dia2[row + i] = 1;
+                List<List<String>> r = dfs2(queens, n, row + 1, col, dia1, dia2);
+                //回溯
+                queens[row] = 0;
+                col[i] = 0;
+                dia1[row - i + n - 1] = 0;
+                dia2[row + i] = 0;
+
+                result.addAll(r);
+            }
+        }
+        return result;
+    }
+
+    private boolean canSet2(int n, int x, int y, int[] col, int[] dia1, int[] dia2) {
+        //正上方
+        if (col[y] == 1) {
+            return false;
+        }
+        //左斜上方
+        if (dia1[x - y + n - 1] == 1) {
+            return false;
+        }
+        //右斜上方
+        if (dia2[x + y] == 1) {
+            return false;
+        }
+        return true;
+    }
+
+    private List<String> makeStr2(int[] queens, int n) {
+        List<String> result = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            StringBuilder sb = new StringBuilder(n);
+            for (int j = 0; j < n; j++) {
+                if (queens[i] == j) {
+                    sb.append('Q');
+                } else {
+                    sb.append('.');
+                }
+            }
+            result.add(sb.toString());
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        NQueens nq = new NQueens();
+        List<List<String>> lists = nq.solveNQueens2(4);
+        for (List<String> list : lists) {
+            System.out.println("===================");
+            for (String s : list) {
+                System.out.println(s);
+            }
+        }
     }
 }
