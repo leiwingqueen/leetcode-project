9730cdc8f97cd6577b2e039adce5c9e19faf729c
liyongquan <496326100@qq.com>
2020-10-23 00:09:41 +0800

    刷题

--- src/main/java/com/liyongquan/dp/Coin.java
+++ src/main/java/com/liyongquan/dp/Coin.java
@@ -2,65 +2,88 @@ package com.liyongquan.dp;
 
 /**
  * 硬币。给定数量不限的硬币，币值为25分、10分、5分和1分，编写代码计算n分有几种表示法。(结果可能会很大，你需要将结果模上1000000007)
- *
+ * <p>
  * 示例1:
- *
- *  输入: n = 5
- *  输出：2
- *  解释: 有两种方式可以凑成总金额:
+ * <p>
+ * 输入: n = 5
+ * 输出：2
+ * 解释: 有两种方式可以凑成总金额:
  * 5=5
  * 5=1+1+1+1+1
- *
+ * <p>
  * 示例2:
- *
- *  输入: n = 10
- *  输出：4
- *  解释: 有四种方式可以凑成总金额:
+ * <p>
+ * 输入: n = 10
+ * 输出：4
+ * 解释: 有四种方式可以凑成总金额:
  * 10=10
  * 10=5+5
  * 10=5+1+1+1+1+1
  * 10=1+1+1+1+1+1+1+1+1+1
- *
+ * <p>
  * 说明：
- *
+ * <p>
  * 注意:
- *
+ * <p>
  * 你可以假设：
- *
- *     0 <= n (总金额) <= 1000000
- *
+ * <p>
+ * 0 <= n (总金额) <= 1000000
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/coin-lcci
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Coin {
     /**
-     * f(n)=f(n-25)+f(n-10)+f(n-5)+f(n-1);
-     * f(0)=0;
+     * n的数据量到百万级，这也就意味我们只能找一个O(n)的解法。
+     * <p>
+     * 思路有点像被背包问题，组合的问题最终转化成 选/不选的问题。
+     * <p>
+     * 假设数组int[] c=[25,10,5,1]。
+     * <p>
+     * 我们有dp表达式：
+     * <p>
+     * f(k,n)定义为只使用前面k个硬币，组装成n的解法。
+     * f(k,n)=f(k-1,n)+f(k,n-c[k])
+     * <p>
+     * 解释：
+     * 不用第k个硬币，只使用前面k-1个硬币--f(k-1,n)
+     * 至少用一个第k个硬币。--f(k,n-c[k])
+     * <p>
+     * 时间复杂度O(4*n)，空间复杂度O(4*n)
+     *
      * @param n
      * @return
      */
     public int waysToChange(int n) {
-        int[] r=new int[n+1];
-        for (int i = 0; i < n+1; i++) {
-            if (i==0) {
-                r[i]=0;
-            }else {
-                r[i]=get(r,i-25)+get(r,i-10)+get(r,i-5)+get(r,i-1);
-            }
-        }
-        return r[n];
-    }
-
-    private int get(int[] r,int i){
-        if(i<0){
+        if (n == 0) {
             return 0;
         }
-        return r[i];
+        int[] coin = new int[]{1, 5, 10, 25};
+        int[][] dp = new int[4][n + 1];
+        //初始化
+        for (int i = 0; i < coin.length; i++) {
+            //这里大家可以想象为什么是1而不是0
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            dp[0][i] = 1;
+        }
+        //dp迭代
+        for (int i = 1; i < coin.length; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (j >= coin[i]) {
+                    dp[i][j] = (dp[i - 1][j] + dp[i][j - coin[i]]) % 1000000007;
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[coin.length - 1][n];
     }
 
     public static void main(String[] args) {
-        Coin coin=new Coin();
+        Coin coin = new Coin();
         int i = coin.waysToChange(6);
         System.out.println(i);
     }
--- src/main/java/com/liyongquan/twopointer/LongPressedName.java
+++ src/main/java/com/liyongquan/twopointer/LongPressedName.java
@@ -82,4 +82,41 @@ public class LongPressedName {
         }
         return true;
     }
+
+    /**
+     * 根据题意能够分析得到：字符串 \textit{typed}typed 的每个字符，有且只有两种「用途」：
+     * <p>
+     * 作为 \textit{name}name 的一部分。此时会「匹配」\textit{name}name 中的一个字符
+     * <p>
+     * 作为长按键入的一部分。此时它应当与前一个字符相同。
+     * <p>
+     * 如果 \textit{typed}typed 中存在一个字符，它两个条件均不满足，则应当直接返回 \textit{false}false；否则，当 \textit{typed}typed 扫描完毕后，我们再检查 \textit{name}name 的每个字符是否都被「匹配」了。
+     * <p>
+     * 实现上，我们使用两个下标 i,ji,j 追踪 \textit{name}name 和 \textit{typed}typed 的位置。
+     * <p>
+     * 当 \textit{name}[i]=\textit{typed}[j]name[i]=typed[j] 时，说明两个字符串存在一对匹配的字符，此时将 i,ji,j 都加 11。
+     * <p>
+     * 否则，如果 \textit{typed}[j]=\textit{typed}[j-1]typed[j]=typed[j−1]，说明存在一次长按键入，此时只将 jj 加 11。
+     * <p>
+     * 最后，如果 i=\textit{name}.\textit{length}i=name.length，说明 \textit{name}name 的每个字符都被「匹配」了。
+     * <p>
+     * 作者：LeetCode-Solution
+     * 链接：https://leetcode-cn.com/problems/long-pressed-name/solution/chang-an-jian-ru-by-leetcode-solution/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     */
+    public boolean isLongPressedName2(String name, String typed) {
+        int i = 0, j = 0;
+        while (j < typed.length()) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) {
+                i++;
+                j++;
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
+                j++;
+            } else {
+                return false;
+            }
+        }
+        return i == name.length();
+    }
 }
