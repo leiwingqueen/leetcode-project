83f9fdb37e8a92390444d3b01a592eabb5ee1b63
liyongquan <496326100@qq.com>
2020-11-16 23:35:52 +0800

    最佳直线

--- src/main/java/com/liyongquan/dp/BestLine.java
+++ src/main/java/com/liyongquan/dp/BestLine.java
@@ -1,5 +1,6 @@
 package com.liyongquan.dp;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 /**
@@ -72,6 +73,8 @@ public class BestLine {
      * 那么我们如果确定一条直接经过多少个点？
      * <p>
      * 最简单的做法是穷举n*(n-1)种情况，使用a,b作为key。value为经过的点。穷举完成就得到最终结果了。
+     * <p>
+     * 这种解法有精度问题
      *
      * @param points
      * @return
@@ -83,8 +86,8 @@ public class BestLine {
         int[] result = new int[2];
         for (int i = 0; i < points.length - 1; i++) {
             for (int j = i + 1; j < points.length; j++) {
-                double[] line = getLine(points[i], points[j]);
-                String key = line[0] + "," + line[1];
+                long[] line = getLine2(points[i], points[j]);
+                String key = hash(line[0], line[1], line[2]);
                 Set<Integer> set = map.getOrDefault(key, new HashSet<>());
                 set.add(i);
                 set.add(j);
@@ -92,24 +95,62 @@ public class BestLine {
                 if (!pos.containsKey(key)) {
                     pos.put(key, new int[]{i, j});
                 }
+                //System.out.println("[" + pos.get(key)[0] + "," + pos.get(key)[1] + "]:" + set.size());
                 if (set.size() > max) {
                     max = set.size();
                     result[0] = pos.get(key)[0];
                     result[1] = pos.get(key)[1];
+                    //System.out.println("[" + result[0] + "," + result[1] + "]:" + max);
                 }
             }
         }
         return result;
     }
 
-    private double[] getLine(int[] p1, int[] p2) {
-        double a, b;
+    /**
+     * 这个解法有精度问题
+     *
+     * @param p1
+     * @param p2
+     * @return
+     */
+    public BigDecimal[] getLine(int[] p1, int[] p2) {
+        BigDecimal a, b;
         if (p1[0] == p2[0]) {
-            a = 0;
+            a = BigDecimal.ZERO;
         } else {
-            a = (double) ((p2[1] - p1[1])) / (p2[0] - p2[0]);
+            a = BigDecimal.valueOf(p2[1] - p1[1]).divide(BigDecimal.valueOf(p2[0] - p1[0]));
         }
-        b = p1[1] - a * p1[0];
-        return new double[]{a, b};
+        b = BigDecimal.valueOf(p1[1]).subtract(a.multiply(BigDecimal.valueOf(p1[0])));
+        return new BigDecimal[]{a, b};
+    }
+
+
+    /**
+     * 2）对于任意两点(X1,Y1)、(X2,Y2)，都有 A = Y2 - Y1 B = X1 - X2 C = X2Y1 - X1Y2
+     * <p>
+     * 另外还要取最大公约数
+     *
+     * @param p1
+     * @param p2
+     * @return
+     */
+    private long[] getLine2(int[] p1, int[] p2) {
+        long a = p2[1] - p1[1];
+        long b = p1[0] - p1[0];
+        long c = p2[0] * p1[1] - p1[0] * p2[1];
+        long gcd = gcd(gcd(a, b), c);
+        return new long[]{a / gcd, b / gcd, c / gcd};
+    }
+
+    private long gcd(long a, long b) {
+        if (a == 0) {
+            return 1;
+        }
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    private String hash(long a, long b, long c) {
+        return a + "#" + b + "#" + c;
     }
 }
--- src/test/java/com/liyongquan/dp/BestLineTest.java
+++ src/test/java/com/liyongquan/dp/BestLineTest.java
@@ -3,6 +3,8 @@ package com.liyongquan.dp;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
 
 public class BestLineTest {
@@ -25,5 +27,16 @@ public class BestLineTest {
             System.out.print(i + ",");
         }
         Assert.assertArrayEquals(new int[]{0, 2}, ints);
+    }
+
+    //[[-38935,27124],[-39837,19604],[-7086,42194],[-11571,-23257],[115,-23257],[20229,5976],[24653,-18488],[11017,21043],[-9353,16550],[-47076,15237],[-36686,42194],[-17704,1104],[31067,7368],[-20882,42194],[-19107,-10597],[-14898,24506],[-20801,42194],[-52268,40727],[-14042,42194],[-23254,42194],[-30837,-53882],[1402,801],[-33961,-984],[-6411,42194],[-12210,22901],[-8213,-19441],[-26939,20810],[30656,-23257],[-27195,21649],[-33780,2717],[23617,27018],[12266,3608]]
+    @Test
+    public void getLine() {
+        BigDecimal[] line = bl.getLine(new int[]{-7086, 42194}, new int[]{-36686, 42194});
+        System.out.println(line[0]);
+        System.out.println(line[0] + "," + line[1]);
+
+        line = bl.getLine(new int[]{-7086, 42194}, new int[]{-6411, 42194});
+        System.out.println(line[0] + "," + line[1]);
     }
 }
