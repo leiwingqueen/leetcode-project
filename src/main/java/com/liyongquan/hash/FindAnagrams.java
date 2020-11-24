a4ea4fb451f92c7af536d9fd84baf3ad1642feb8
liyongquan <496326100@qq.com>
2020-11-24 09:56:40 +0800

    字母异位子串

--- src/main/java/com/liyongquan/hash/FindAnagrams.java
+++ src/main/java/com/liyongquan/hash/FindAnagrams.java
@@ -1,9 +1,6 @@
 package com.liyongquan.hash;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 给定一个字符串 s 和一个非空字符串 p，找到 s 中所有是 p 的字母异位词的子串，返回这些子串的起始索引。
@@ -82,5 +79,51 @@ public class FindAnagrams {
             }
         }
         return result;
+    }
+
+    /**
+     * 作者：jiang-mi-2
+     *     链接：https://leetcode-cn.com/problems/find-all-anagrams-in-a-string/solution/javayou-hua-labuladongda-lao-hua-dong-chuang-kou-t/
+     *     来源：力扣（LeetCode）
+     *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     *     增加一个total变量，这个做法比较巧妙
+     * @param s
+     * @param p
+     * @return
+     */
+    public List<Integer> findAnagrams2(String s, String p) {
+        if(s == null || s.length() == 0) return new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        int[] needs = new int[26]; //由于都是小写字母，因此直接用26个长度的数组代替原来的HashMap
+        int[] window = new int[26];
+        int left = 0, right = 0, total = p.length(); //用total检测窗口中是否已经涵盖了p中的字符
+        for(char ch : p.toCharArray()){
+            needs[ch - 'a'] ++;
+        }
+        while(right < s.length()){
+            char chr = s.charAt(right);
+            if(needs[chr - 'a'] > 0){
+                window[chr - 'a'] ++;
+                if(window[chr - 'a'] <= needs[chr - 'a']){
+                    total --;
+                }
+            }
+            while(total == 0){
+                if(right-left+1 == p.length()){
+                    res.add(left);
+                }
+                char chl = s.charAt(left);
+                if(needs[chl - 'a'] > 0){
+                    window[chl - 'a'] --;
+                    if(window[chl - 'a'] < needs[chl - 'a']){
+                        total ++;
+                    }
+                }
+                left ++;
+            }
+            right ++;
+        }
+        return res;
     }
 }
