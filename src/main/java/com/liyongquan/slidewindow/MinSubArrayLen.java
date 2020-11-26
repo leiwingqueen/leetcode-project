eba1f9f0015d4bb1e98897499d7f94698b42d69b
liyongquan <496326100@qq.com>
2020-11-26 11:09:59 +0800

    最小子数组-代码优化

--- src/main/java/com/liyongquan/slidewindow/MinSubArrayLen.java
+++ src/main/java/com/liyongquan/slidewindow/MinSubArrayLen.java
@@ -28,7 +28,7 @@ public class MinSubArrayLen {
      * 问题，如何证明滑动窗口的时间复杂度是O(n)?
      * <p>
      * 对于任意一个指针，只会往一个方向移动。两个指针的最坏的时间复杂度是O(2n)
-     *
+     * <p>
      * 空间复杂度O(1)
      *
      * @param s
@@ -58,4 +58,31 @@ public class MinSubArrayLen {
         }
         return min > nums.length ? 0 : min;
     }
+
+    /**
+     * 滑动窗口的写法还能简化一下
+     *
+     * @param s
+     * @param nums
+     * @return
+     */
+    public int minSubArrayLen2(int s, int[] nums) {
+        int l = 0, r = 0, sum = 0, min = nums.length + 1;
+        while (r < nums.length) {
+            //右边界移动
+            sum += nums[r];
+            r++;
+            //左边界移动
+            while (sum >= s) {
+                //更新结果
+                int len = r - l;
+                min = Math.min(min, len);
+                sum -= nums[l];
+                l++;
+            }
+        }
+        return min > nums.length ? 0 : min;
+    }
+
+    //还有一种方案是前缀树+二分查找的方案，也是比较有意思
 }
