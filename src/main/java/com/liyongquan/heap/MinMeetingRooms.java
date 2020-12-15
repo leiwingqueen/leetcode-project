6108967ac7498b8a33ec852c9eb4da60f753710a
liyongquan <496326100@qq.com>
2020-12-15 18:55:20 +0800

    会议室

---
+++ src/main/java/com/liyongquan/heap/MinMeetingRooms.java
@@ -0,0 +1,24 @@
+package com.liyongquan.heap;
+
+/**
+ * 给定一个会议时间安排的数组，每个会议时间都会包括开始和结束的时间 [[s1,e1],[s2,e2],...] (si < ei)，为避免会议冲突，同时要考虑充分利用会议室资源，请你计算至少需要多少间会议室，才能满足这些会议安排。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [[0, 30],[5, 10],[15, 20]]
+ * 输出: 2
+ * 示例 2:
+ * <p>
+ * 输入: [[7,10],[2,4]]
+ * 输出: 1
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/meeting-rooms-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MinMeetingRooms {
+    public int minMeetingRooms(int[][] intervals) {
+        return 0;
+    }
+}
