ab85df3a7465a18f1fd252b46d4364b33c84a0d6
liyongquan <496326100@qq.com>
2021-01-03 13:14:36 +0800

    链表分割

--- src/main/java/com/liyongquan/linklist/ListNode.java
+++ src/main/java/com/liyongquan/linklist/ListNode.java
@@ -7,4 +7,17 @@ public class ListNode {
     public ListNode(int x) {
         val = x;
     }
+
+    /**
+     * 自己定义的构造方法，方便跑单元测试
+     */
+    public static ListNode build(int[] nodes) {
+        ListNode dummy = new ListNode(0);
+        ListNode cur = dummy;
+        for (int node : nodes) {
+            cur.next = new ListNode(node);
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
 }
