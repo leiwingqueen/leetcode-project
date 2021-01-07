7aeed3fba5bb79ddbd7144d09e14df9145836015
liyongquan <496326100@qq.com>
2021-01-07 18:51:30 +0800

    字符串相似

---
+++ src/test/java/com/liyongquan/hash/AreSentencesSimilarTest.java
@@ -0,0 +1,17 @@
+package com.liyongquan.hash;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AreSentencesSimilarTest {
+
+    /**
+     * ["an","extraordinary","meal"]
+     * ["one","good","dinner"]
+     * [["great","good"],["extraordinary","good"],["well","good"],["wonderful","good"],["excellent","good"],["fine","good"],["nice","good"],["any","one"],["some","one"],["unique","one"],["the","one"],["an","one"],["single","one"],["a","one"],["truck","car"],["wagon","car"],["automobile","car"],["auto","car"],["vehicle","car"],["entertain","have"],["drink","have"],["eat","have"],["take","have"],["fruits","meal"],["brunch","meal"],["breakfast","meal"],["food","meal"],["dinner","meal"],["super","meal"],["lunch","meal"],["possess","own"],["keep","own"],["have","own"],["extremely","very"],["actually","very"],["really","very"],["super","very"]]
+     */
+    @Test
+    public void areSentencesSimilar() {
+    }
+}
