977a1fb5f84c43f7510dff8cb1d883a680fb3306
liyongquan <496326100@qq.com>
2021-02-08 17:59:07 +0800

    每日一题

---
+++ src/test/java/com/liyongquan/slidewindow/MaxTurbulenceSizeTest.java
@@ -0,0 +1,16 @@
+package com.liyongquan.slidewindow;
+
+import junit.framework.TestCase;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+
+@Slf4j
+public class MaxTurbulenceSizeTest extends TestCase {
+    private MaxTurbulenceSize mt = new MaxTurbulenceSize();
+
+    public void testMaxTurbulenceSize() {
+        int res = mt.maxTurbulenceSize(new int[]{0,1,1,0,1,0,1,1,0,0});
+        log.info("{}", res);
+        Assert.assertEquals(5, res);
+    }
+}
