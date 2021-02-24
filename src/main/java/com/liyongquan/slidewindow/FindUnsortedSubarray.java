9f515fa0a097f7c20154c964726e31129201cc4e
liyongquan <496326100@qq.com>
2021-02-25 00:33:59 +0800

    每日一题

---
+++ src/main/java/com/liyongquan/slidewindow/FindUnsortedSubarray.java
@@ -0,0 +1,49 @@
+package com.liyongquan.slidewindow;
+
+/**
+ * 581. 最短无序连续子数组
+ * <p>
+ * 给你一个整数数组 nums ，你需要找出一个 连续子数组 ，如果对这个子数组进行升序排序，那么整个数组都会变为升序排序。
+ * <p>
+ * 请你找出符合题意的 最短 子数组，并输出它的长度。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,6,4,8,10,9,15]
+ * 输出：5
+ * 解释：你只需要对 [6, 4, 8, 10, 9] 进行升序排序，那么整个表都会变为升序排序。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,3,4]
+ * 输出：0
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1]
+ * 输出：0
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 104
+ * -105 <= nums[i] <= 105
+ *  
+ * <p>
+ * 进阶：你可以设计一个时间复杂度为 O(n) 的解决方案吗？
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shortest-unsorted-continuous-subarray
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindUnsortedSubarray {
+    /**
+     * 相当于找到一个最短的子串
+     *
+     * @param nums
+     * @return
+     */
+    public int findUnsortedSubarray(int[] nums) {
+        return 0;
+    }
+}
