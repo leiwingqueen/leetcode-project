0432e43007d7e7b71da41df81ad107cdda7c019e
liyongquan <496326100@qq.com>
2021-03-05 15:04:10 +0800

    工作计划

---
+++ src/main/java/com/liyongquan/dp/MaximalRectangle.java
@@ -0,0 +1,52 @@
+package com.liyongquan.dp;
+
+/**
+ * 给定一个仅包含 0 和 1 、大小为 rows x cols 的二维二进制矩阵，找出只包含 1 的最大矩形，并返回其面积。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+ * 输出：6
+ * 解释：最大矩形如上图所示。
+ * 示例 2：
+ * <p>
+ * 输入：matrix = []
+ * 输出：0
+ * 示例 3：
+ * <p>
+ * 输入：matrix = [["0"]]
+ * 输出：0
+ * 示例 4：
+ * <p>
+ * 输入：matrix = [["1"]]
+ * 输出：1
+ * 示例 5：
+ * <p>
+ * 输入：matrix = [["0","0"]]
+ * 输出：0
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * rows == matrix.length
+ * cols == matrix[0].length
+ * 0 <= row, cols <= 200
+ * matrix[i][j] 为 '0' 或 '1'
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/maximal-rectangle
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class MaximalRectangle {
+    /**
+     * 有点难，暂时没想到解法...
+     * @param matrix
+     * @return
+     */
+    public int maximalRectangle(char[][] matrix) {
+        return 0;
+    }
+}
