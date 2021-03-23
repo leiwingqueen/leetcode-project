1956750bfd353c0adaf3f2439815e9de10d4e83c
liyongquan <496326100@qq.com>
2021-03-23 17:34:16 +0800

    增加数据统计

--- src/test/java/com/liyongquan/design/PenguinTrap4Test.java
+++ src/test/java/com/liyongquan/design/PenguinTrap4Test.java
@@ -11,6 +11,8 @@ public class PenguinTrap4Test {
 
     private PenguinTrap4 pt = new PenguinTrap4();
 
+    private Random random = new Random();
+
     @Test
     public void knock() {
         Hex[] blocks = {
@@ -46,6 +48,71 @@ public class PenguinTrap4Test {
         log.info("掉落的总冰块数量:{}", cnt);
     }
 
+    /**
+     * 统计敲击多少次会完全掉落
+     */
+    @Test
+    public void sysbench() {
+        int sum = 0;
+        for (int i = 0; i < 1000; i++) {
+            Map<Hex, BlockType> map = build(RADIUS);
+            Optional<Hex> opt = pick(map);
+            int cnt = 0;
+            while (opt.isPresent()) {
+                pt.knock(map, Arrays.asList(opt.get()));
+                opt = pick(map);
+                cnt++;
+            }
+            sum += cnt;
+            log.info("敲击次数:{}", cnt);
+        }
+        log.info("平均敲击次数:{}", sum / 1000);
+    }
+
+    /**
+     * 假设企鹅在中间的平均敲击次数
+     */
+    @Test
+    public void sysbench2() {
+        int sum = 0;
+        int total = 1000;
+        Hex center = new Hex(0, 0, 0);
+        Map<Integer, Integer> cntMap = new HashMap<>();
+        for (int i = 0; i < total; i++) {
+            Map<Hex, BlockType> map = build(RADIUS);
+            int cnt = 0;
+            while (map.get(center) == BlockType.BLOCK) {
+                Optional<Hex> opt = pick(map);
+                while (opt.get().equals(center)) {
+                    opt = pick(map);
+                }
+                pt.knock(map, Arrays.asList(opt.get()));
+                cnt++;
+            }
+            sum += cnt;
+            //log.info("敲击次数:{}", cnt);
+            cntMap.put(cnt, cntMap.getOrDefault(cnt, 0) + 1);
+        }
+        log.info("平均敲击次数:{}", sum / total);
+        for (Map.Entry<Integer, Integer> entry : cntMap.entrySet()) {
+            log.info("敲击次数{}:数量{}", entry.getKey(), entry.getValue());
+        }
+    }
+
+    private Optional<Hex> pick(Map<Hex, BlockType> map) {
+        List<Hex> list = new ArrayList<>();
+        for (Map.Entry<Hex, BlockType> entry : map.entrySet()) {
+            if (entry.getValue() == BlockType.BLOCK) {
+                list.add(entry.getKey());
+            }
+        }
+        if (list.size() == 0) {
+            return Optional.empty();
+        }
+        int i = random.nextInt(list.size());
+        return Optional.of(list.get(i));
+    }
+
     private Map<Hex, BlockType> build(int radius) {
         Map<Hex, BlockType> map = new HashMap<>();
         //初始化中心点
@@ -64,7 +131,7 @@ public class PenguinTrap4Test {
         for (Hex hex : hexes) {
             map.put(hex, BlockType.WALL);
         }
-        log.info("总冰块数量:{}", cnt);
+        //log.info("总冰块数量:{}", cnt);
         return map;
     }
 
