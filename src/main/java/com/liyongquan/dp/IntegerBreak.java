a6e6fca2cc6b7d7ef2670803973228efcb4599a1
leiwingqueen <leiwingqueen@qq.com>
2021-03-28 22:10:51 +0800

    整数拆分

---
+++ src/main/java/com/liyongquan/dp/IntegerBreak.java
@@ -0,0 +1,44 @@
+package com.liyongquan.dp;
+
+/**
+ * 343. 整数拆分
+ * <p>
+ * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 2
+ * 输出: 1
+ * 解释: 2 = 1 + 1, 1 × 1 = 1。
+ * 示例 2:
+ * <p>
+ * 输入: 10
+ * 输出: 36
+ * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
+ * 说明: 你可以假设 n 不小于 2 且不大于 58。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/integer-break
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class IntegerBreak {
+    /**
+     * 从周赛过来，先完成一个最简单的dp
+     *
+     * @param n
+     * @return
+     */
+    public int integerBreak(int n) {
+        //多保留一位方便理解
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            int max = 0;
+            for (int j = 1; j < i; j++) {
+                max = Math.max(max, Math.max(j * (i - j), j * dp[i - j]));
+            }
+            dp[i] = max;
+        }
+        return dp[n];
+    }
+}
