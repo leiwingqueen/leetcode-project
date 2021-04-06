f109fff80a390c795a33687db89d6ae4f87e6610
leiwingqueen <leiwingqueen@qq.com>
2021-04-06 11:42:48 +0800

    2021比赛刷题

--- src/main/java/com/liyongquan/weeklycontest/spring2021/MagicTower.java
+++ src/main/java/com/liyongquan/weeklycontest/spring2021/MagicTower.java
@@ -1,8 +1,39 @@
 package com.liyongquan.weeklycontest.spring2021;
 
-import java.util.LinkedList;
 import java.util.PriorityQueue;
 
+/**
+ * LCP 30. 魔塔游戏
+ * <p>
+ * 小扣当前位于魔塔游戏第一层，共有 N 个房间，编号为 0 ~ N-1。每个房间的补血道具/怪物对于血量影响记于数组 nums，其中正数表示道具补血数值，即血量增加对应数值；负数表示怪物造成伤害值，即血量减少对应数值；0 表示房间对血量无影响。
+ * <p>
+ * 小扣初始血量为 1，且无上限。假定小扣原计划按房间编号升序访问所有房间补血/打怪，为保证血量始终为正值，小扣需对房间访问顺序进行调整，每次仅能将一个怪物房间（负数的房间）调整至访问顺序末尾。请返回小扣最少需要调整几次，才能顺利访问所有房间。若调整顺序也无法访问完全部房间，请返回 -1。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [100,100,100,-250,-60,-140,-50,-50,100,150]
+ * <p>
+ * 输出：1
+ * <p>
+ * 解释：初始血量为 1。至少需要将 nums[3] 调整至访问顺序末尾以满足要求。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [-200,-300,400,0]
+ * <p>
+ * 输出：-1
+ * <p>
+ * 解释：调整访问顺序也无法完成全部房间的访问。
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 10^5
+ * -10^5 <= nums[i] <= 10^5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/p0NxJO
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class MagicTower {
     /**
      * 不通过
@@ -38,7 +69,7 @@ public class MagicTower {
         int cnt = 0;
         PriorityQueue<Long> pq = new PriorityQueue<>();
         for (int i = 0; i < len; i++) {
-            pq.add((long)nums[i]);
+            pq.add((long) nums[i]);
             blood += nums[i];
             if (blood <= 0) {
                 if (pq.isEmpty() || pq.peek() >= 0) {
--- src/main/java/com/liyongquan/weeklycontest/spring2021/OrchestraLayout.java
+++ src/main/java/com/liyongquan/weeklycontest/spring2021/OrchestraLayout.java
@@ -1,6 +1,48 @@
 package com.liyongquan.weeklycontest.spring2021;
 
+// LCP 29. 乐团站位
+// 某乐团的演出场地可视作 `num * num` 的二维矩阵 `grid`（左上角坐标为 `[0,0]`)，每个位置站有一位成员。乐团共有 `9` 种乐器，乐
+//器编号为 `1~9`，每位成员持有 `1` 个乐器。
+//
+//为保证声乐混合效果，成员站位规则为：自 `grid` 左上角开始顺时针螺旋形向内循环以 `1，2，...，9` 循环重复排列。例如当 num = `5` 时
+//，站位如图所示
+//
+//![image.png](https://pic.leetcode-cn.com/1616125411-WOblWH-image.png)
+//
+//
+//请返回位于场地坐标 [`Xpos`,`Ypos`] 的成员所持乐器编号。
+//
+//**示例 1：**
+//>输入：`num = 3, Xpos = 0, Ypos = 2`
+//>
+//>输出：`3`
+//>
+//>解释：
+//![image.png](https://pic.leetcode-cn.com/1616125437-WUOwsu-image.png)
+//
+//
+//**示例 2：**
+//>输入：`num = 4, Xpos = 1, Ypos = 2`
+//>
+//>输出：`5`
+//>
+//>解释：
+//![image.png](https://pic.leetcode-cn.com/1616125453-IIDpxg-image.png)
+//
+//
+//**提示：**
+//- `1 <= num <= 10^9`
+//- `0 <= Xpos, Ypos < num` 👍 17 👎 0
+
 public class OrchestraLayout {
+    /**
+     * 不通过，写法有点绕
+     *
+     * @param num
+     * @param xPos
+     * @param yPos
+     * @return
+     */
     public int orchestraLayout(int num, int xPos, int yPos) {
         double middle = (num - 1) / 2d;
         int round = num / 2 - (int) Math.ceil(Math.max(Math.abs(xPos - middle), Math.abs(yPos - middle)));
@@ -11,8 +53,9 @@ public class OrchestraLayout {
             if (edge == 1) {
                 cnt += 1;
             } else {
-                cnt += (edge - 1) * 4;
+                cnt += ((edge - 1) * 4) % 9;
             }
+            cnt %= 9;
         }
         //x,y所在的环
         int edge = num - round * 2;
@@ -35,5 +78,45 @@ public class OrchestraLayout {
             }
         }
         return cnt % 9 == 0 ? 9 : cnt % 9;
+    }
+
+    /**
+     * 本质上求[x,y]所在的序号
+     *
+     * @param num
+     * @param xPos
+     * @param yPos
+     * @return
+     */
+    public int orchestraLayout2(int num, int xPos, int yPos) {
+        //求所在的层数(距离4条边的最近的距离)
+        long round = Math.min(xPos, Math.min(yPos, Math.min(num - 1 - xPos, num - 1 - yPos)));
+        //一共的层数[0,ceil(num/2)]
+        //int maxRound = (num + 1) / 2;
+        //前面每个环的数量 f(i)=(n-i*2-1)*4;
+        //数学公式推导得到前i个环的总和为f(i)=4*n*(i+1)-4*(i+1)-i*(i+1)，注意这里有可能溢出，我们需要先取mod
+        //int cnt = (4 * num * round - 4 * round - 4 * (round - 1) * round) % 9;
+        long cnt = ((round % 9) * ((4 * num - 4 * round) % 9)) % 9;
+        //x,y所在的环
+        long edge = num - round * 2;
+        if (edge == 1) {
+            cnt += 1;
+        } else {
+            //左上角的坐标
+            long x0 = round, y0 = round;
+            if (x0 == xPos) {
+                cnt += yPos - y0 + 1;
+            } else if (y0 + edge - 1 == yPos) {
+                cnt += edge - 1;
+                cnt += xPos - x0 + 1;
+            } else if (x0 + edge - 1 == xPos) {
+                cnt += 2 * (edge - 1);
+                cnt += Math.abs(y0 + edge - 1 - yPos) + 1;
+            } else {
+                cnt += 3 * (edge - 1);
+                cnt += Math.abs(xPos - (x0 + edge - 1)) + 1;
+            }
+        }
+        return cnt % 9 == 0 ? 9 : (int) (cnt % 9);
     }
 }
