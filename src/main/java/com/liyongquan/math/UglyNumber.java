787fe7923aad60d2055798bbc8181566da4a2b0c
leiwingqueen <leiwingqueen@qq.com>
2021-04-10 00:28:00 +0800

    丑数2

---
+++ src/main/java/com/liyongquan/math/UglyNumber.java
@@ -0,0 +1,60 @@
+package com.liyongquan.math;
+
+/**
+ * 263. 丑数
+ * <p>
+ * 给你一个整数 n ，请你判断 n 是否为 丑数 。如果是，返回 true ；否则，返回 false 。
+ * <p>
+ * 丑数 就是只包含质因数 2、3 和/或 5 的正整数。
+ * <p>
+ *  
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：n = 6
+ * 输出：true
+ * 解释：6 = 2 × 3
+ * 示例 2：
+ * <p>
+ * 输入：n = 8
+ * 输出：true
+ * 解释：8 = 2 × 2 × 2
+ * 示例 3：
+ * <p>
+ * 输入：n = 14
+ * 输出：false
+ * 解释：14 不是丑数，因为它包含了另外一个质因数 7 。
+ * 示例 4：
+ * <p>
+ * 输入：n = 1
+ * 输出：true
+ * 解释：1 通常被视为丑数。
+ *  
+ * <p>
+ * 提示：
+ * <p>
+ * -231 <= n <= 231 - 1
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/ugly-number
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class UglyNumber {
+    public boolean isUgly(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else if (n % 3 == 0) {
+                n /= 3;
+            } else if (n % 5 == 0) {
+                n /= 5;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
