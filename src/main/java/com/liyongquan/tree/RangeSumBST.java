95e3120019ecd571e35d382a48fc956ba0348ecf
leiwingqueen <leiwingqueen@qq.com>
2021-04-27 12:16:14 +0800

    每日一题-双解法

--- src/main/java/com/liyongquan/tree/RangeSumBST.java
+++ src/main/java/com/liyongquan/tree/RangeSumBST.java
@@ -1,5 +1,8 @@
 package com.liyongquan.tree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 给定二叉搜索树的根结点 root，返回值位于范围 [low, high] 之间的所有结点的值的和。
  * <p>
@@ -48,5 +51,61 @@ public class RangeSumBST {
         } else {
             return root.val + rangeSumBST(root.left, low, root.val - 1) + rangeSumBST(root.right, root.val + 1, high);
         }
+    }
+
+    /**
+     * 上面基础稍微优化一下
+     *
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public int rangeSumBST2(TreeNode root, int low, int high) {
+        if (root == null || low > high) {
+            return 0;
+        }
+        if (root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        } else if (root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        } else {
+            return root.val + rangeSumBST(root.left, low, root.val - 1) + rangeSumBST(root.right, root.val + 1, high);
+        }
+    }
+
+    /**
+     * bfs
+     *
+     * @param root
+     * @param low
+     * @param high
+     * @return
+     */
+    public int rangeSumBST3(TreeNode root, int low, int high) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            if (poll.val < low) {
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            } else if (poll.val > high) {
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+            } else {
+                sum += poll.val;
+                if (poll.left != null) {
+                    queue.add(poll.left);
+                }
+                if (poll.right != null) {
+                    queue.add(poll.right);
+                }
+            }
+        }
+        return sum;
     }
 }
