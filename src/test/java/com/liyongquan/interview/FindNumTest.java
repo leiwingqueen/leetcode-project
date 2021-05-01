2642066eb353e1a4728102e14133fad616f7ff3a
leiwingqueen <leiwingqueen@qq.com>
2021-05-02 00:36:59 +0800

    字节面试题

---
+++ src/test/java/com/liyongquan/interview/FindNumTest.java
@@ -0,0 +1,30 @@
+package com.liyongquan.interview;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@Slf4j
+public class FindNumTest {
+    private FindNum fn = new FindNum();
+
+    @Test
+    public void find() {
+        List<Integer> res = fn.find(new int[]{9, 8, 7, 3, 4, 2, 1});
+        for (Integer re : res) {
+            log.info("{}", re);
+        }
+    }
+
+    @Test
+    public void test2() {
+        List<Integer> res = fn.find(new int[]{2, 9, 8, 7, 3, 4, 2, 6, 1});
+        for (Integer re : res) {
+            log.info("{}", re);
+        }
+    }
+}
