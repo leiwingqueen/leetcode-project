a68af304e67202273523a0647af1300a2cd5fcb7
leiwingqueen <leiwingqueen@qq.com>
2021-05-03 00:56:39 +0800

    数字翻转

--- src/main/java/com/liyongquan/math/Reverse.java
+++ src/main/java/com/liyongquan/math/Reverse.java
@@ -1,32 +1,51 @@
 package com.liyongquan.math;
 
-import com.liyongquan.square.Shape;
-
 public class Reverse {
     public int reverse(int x) {
-        int positive=x>=0?1:0;
-        int[] a=new int[10];
-        int index=0;
-        x=x>=0?x:-x;
-        while (x>0){
-            int mode=x%10;
-            x=x/10;
-            a[index]=mode;
+        int positive = x >= 0 ? 1 : 0;
+        int[] a = new int[10];
+        int index = 0;
+        x = x >= 0 ? x : -x;
+        while (x > 0) {
+            int mode = x % 10;
+            x = x / 10;
+            a[index] = mode;
             index++;
         }
-        long out=0;
+        long out = 0;
         for (int i = 0; i < index; i++) {
-            out=out*10;
-            out+=a[i];
+            out = out * 10;
+            out += a[i];
         }
-        if (out>Integer.MAX_VALUE) {
-            out=0;
+        if (out > Integer.MAX_VALUE) {
+            out = 0;
         }
-        return positive==1?(int)out:-(int)out;
+        return positive == 1 ? (int) out : -(int) out;
     }
 
+    public int reverse2(int x) {
+        if (x == Integer.MIN_VALUE) {
+            return 0;
+        }
+        boolean positive = true;
+        if (x < 0) {
+            positive = false;
+            x = -x;
+        }
+        long num = 0;
+        while (x != 0) {
+            num = num * 10 + x % 10;
+            x /= 10;
+        }
+        if (num > Integer.MAX_VALUE) {
+            return 0;
+        }
+        return positive ? (int) num : -(int) num;
+    }
+
+
     public static void main(String[] args) {
-        Reverse reverse=new Reverse();
+        Reverse reverse = new Reverse();
         System.out.println(reverse.reverse(-120));
     }
 }
