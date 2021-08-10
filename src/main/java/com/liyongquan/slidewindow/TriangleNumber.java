8b27b5b86220aa4f7fc2f3e75f878d6db5751933
leiwingqueen <leiwingqueen@qq.com>
2021-08-10 19:30:27 +0800

    优化解法

--- src/main/java/com/liyongquan/slidewindow/TriangleNumber.java
+++ src/main/java/com/liyongquan/slidewindow/TriangleNumber.java
@@ -68,7 +68,7 @@ public class TriangleNumber {
     }
 
     /**
-     * 暴力解法
+     * 双指针
      *
      * @param nums
      * @return
@@ -82,6 +82,7 @@ public class TriangleNumber {
         int res = 0;
         for (int i = 0; i < len - 2; i++) {
             //稍微优化了一点点，当j向后移动的时候，k不需要重新从j+1开始扫描
+            //第二层循环里面是典型的双指针的写法，由于两个指针都是单向移动的，相当于每个指针在每个下标下只会移动一次。所以时间复杂度还是O(n)(移动两次,2n的复杂度还是O(n))
             int k = i + 2;
             for (int j = i + 1; j < len - 1; j++) {
                 if (k <= j) {
@@ -94,5 +95,47 @@ public class TriangleNumber {
             }
         }
         return res;
+    }
+
+    /**
+     * 排序+二分
+     *
+     * @param nums
+     * @return
+     */
+    public int triangleNumber3(int[] nums) {
+        int len = nums.length;
+        if (len < 3) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int res = 0;
+        for (int i = 0; i < len - 2; i++) {
+            for (int j = i + 1; j < len - 1; j++) {
+                //这里使用二分查找
+                int k = search(nums, nums[i] + nums[j], j + 1);
+                res += k - j - 1;
+            }
+        }
+        return res;
+    }
+
+    private int search(int[] nums, int sum, int start) {
+        int l = start, r = nums.length - 1;
+        if (nums[start] >= sum) {
+            return start;
+        }
+        if (nums[nums.length - 1] < sum) {
+            return nums.length;
+        }
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < sum) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l;
     }
 }
