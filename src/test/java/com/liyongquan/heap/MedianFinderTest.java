b00c6edfc7b0419cf835edf0a9ed75ab1d9ddd4f
leiwingqueen <leiwingqueen@qq.com>
2021-08-27 21:22:48 +0800

    每日一题

---
+++ src/test/java/com/liyongquan/heap/MedianFinderTest.java
@@ -0,0 +1,22 @@
+package com.liyongquan.heap;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author liyongquan
+ * @date 2021/8/27
+ */
+@Slf4j
+public class MedianFinderTest {
+    private MedianFinder mf = new MedianFinder();
+
+    @Test
+    public void addNum() {
+        mf.addNum(1);
+        mf.addNum(2);
+        mf.addNum(3);
+    }
+}
