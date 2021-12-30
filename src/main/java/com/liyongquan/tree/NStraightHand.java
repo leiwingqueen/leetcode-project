5551a1062a893188967e7bab18c47369a5f8c842
leiwingqueen <leiwingqueen@qq.com>
2021-12-30 10:30:15 +0800

    一手顺子

---
+++ src/main/java/com/liyongquan/tree/NStraightHand.java
@@ -0,0 +1,80 @@
+package com.liyongquan.tree;
+
+import jdk.nashorn.internal.runtime.linker.LinkerCallSite;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+//Alice 手中有一把牌，她想要重新排列这些牌，分成若干组，使每一组的牌数都是 groupSize ，并且由 groupSize 张连续的牌组成。
+//
+//给你一个整数数组 hand 其中 hand[i] 是写在第 i 张牌，和一个整数 groupSize 。如果她可能重新排列这些牌，返回 true ；否则，返回 false 。
+//
+// 
+//
+//示例 1：
+//
+//输入：hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+//输出：true
+//解释：Alice 手中的牌可以被重新排列为 [1,2,3]，[2,3,4]，[6,7,8]。
+//示例 2：
+//
+//输入：hand = [1,2,3,4,5], groupSize = 4
+//输出：false
+//解释：Alice 手中的牌无法被重新排列成几个大小为 4 的组。
+// 
+//
+//提示：
+//
+//1 <= hand.length <= 104
+//0 <= hand[i] <= 109
+//1 <= groupSize <= hand.length
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode-cn.com/problems/hand-of-straights
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+public class NStraightHand {
+    /**
+     * 时间复杂度O(nlogn)
+     *
+     * @param hand
+     * @param groupSize
+     * @return
+     */
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (int h : hand) {
+            map.put(h, map.getOrDefault(h, 0) + 1);
+        }
+        while (!map.isEmpty()) {
+            int cnt = 0;
+            int pre = -1;
+            List<Integer> del = new ArrayList<>(groupSize);
+            for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+                if (pre < 0 || entry.getKey() == pre + 1) {
+                    pre = entry.getKey();
+                    del.add(entry.getKey());
+                    cnt++;
+                    if (cnt == groupSize) {
+                        break;
+                    }
+                } else {
+                    return false;
+                }
+            }
+            if (cnt < groupSize) {
+                return false;
+            }
+            //更新map
+            for (Integer d : del) {
+                if (map.get(d) == 1) {
+                    map.remove(d);
+                } else {
+                    map.put(d, map.get(d) - 1);
+                }
+            }
+        }
+        return true;
+    }
+}
