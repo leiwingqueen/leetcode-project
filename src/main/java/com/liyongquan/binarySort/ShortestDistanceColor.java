da1d4ee87876b2b2ebae98569cecd78e8889046a
leiwingqueen <leiwingqueen@qq.com>
2022-01-07 11:46:43 +0800

    括号的深度

--- src/main/java/com/liyongquan/binarySort/ShortestDistanceColor.java
+++ src/main/java/com/liyongquan/binarySort/ShortestDistanceColor.java
@@ -92,6 +92,9 @@ public class ShortestDistanceColor {
     }
 
     private int searchRight(List<Integer> arr, int target) {
+        if (arr.size() == 0) {
+            return -1;
+        }
         int l = 0, r = arr.size() - 1;
         while (l < r) {
             int mid = l + (r - l) / 2;
@@ -105,6 +108,9 @@ public class ShortestDistanceColor {
     }
 
     private int searchLeft(List<Integer> arr, int target) {
+        if (arr.size() == 0) {
+            return -1;
+        }
         int l = 0, r = arr.size() - 1;
         while (l < r) {
             int mid = l + (r - l + 1) / 2;
@@ -114,6 +120,6 @@ public class ShortestDistanceColor {
                 r = mid - 1;
             }
         }
-        return l >= 0 ? (arr.get(l) - target) : -1;
+        return l >= 0 ? (target - arr.get(l)) : -1;
     }
 }
