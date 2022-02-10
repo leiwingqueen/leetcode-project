2c232278d952ba56a6509af0f759d33173e50d7b
leiwingqueen <leiwingqueen@qq.com>
2022-02-10 10:59:19 +0800

    最简分数

--- src/main/java/com/liyongquan/math/SimplifiedFractions.java
+++ src/main/java/com/liyongquan/math/SimplifiedFractions.java
@@ -47,7 +47,7 @@ public class SimplifiedFractions {
         List<String> res = new LinkedList<>();
         for (int i = 2; i <= n; i++) {
             for (int j = 1; j < i; j++) {
-                if (check(i, j)) {
+                if (gcd(i, j) != 1) {
                     res.add(j + "/" + i);
                 }
             }
@@ -63,5 +63,10 @@ public class SimplifiedFractions {
             }
         }
         return true;
+    }
+
+    //辗转相除法
+    private int gcd(int a, int b) {
+        return b != 0 ? gcd(b, a % b) : a;
     }
 }
