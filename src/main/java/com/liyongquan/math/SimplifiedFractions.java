da3d8b9ec9913822ddcd4515074c09ae22567607
leiwingqueen <leiwingqueen@qq.com>
2022-02-10 14:10:28 +0800

    bugfix

--- src/main/java/com/liyongquan/math/SimplifiedFractions.java
+++ src/main/java/com/liyongquan/math/SimplifiedFractions.java
@@ -47,7 +47,7 @@ public class SimplifiedFractions {
         List<String> res = new LinkedList<>();
         for (int i = 2; i <= n; i++) {
             for (int j = 1; j < i; j++) {
-                if (gcd(i, j) != 1) {
+                if (gcd(i, j) == 1) {
                     res.add(j + "/" + i);
                 }
             }
