9861d03d040be1d3b6e26653255e20c1e883e098
leiwingqueen <leiwingqueen@qq.com>
2022-02-20 16:29:53 +0800

    周赛最后一题

--- src/main/java/com/liyongquan/weeklycontest/wc281/CoutPairs.java
+++ src/main/java/com/liyongquan/weeklycontest/wc281/CoutPairs.java
@@ -1,7 +1,9 @@
 package com.liyongquan.weeklycontest.wc281;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CoutPairs {
     /**
@@ -29,13 +31,39 @@ public class CoutPairs {
         return cnt;
     }
 
-    private List<int[]> split(int k) {
-        List<int[]> list = new ArrayList<>();
-        for (int i = 1; i <= k; i++) {
-            if (k % i == 0) {
-                list.add(new int[]{i, k / i});
+    public static final int mx = 100_000;
+    public static final List<Integer>[] divisors = new List[mx + 1];
+    static {
+        for (int i = 0; i <= mx; i++) {
+            divisors[i] = new ArrayList<>();
+        }
+        //遍历每个因子
+        for (int i = 1; i <= mx; i++) {
+            //遍历因子的倍数
+            for (int j = i; j <= mx; j += i) {
+                divisors[j].add(i);
             }
         }
-        return list;
+    }
+
+    public long coutPairs2(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        long cnt = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int gcd = gcd(nums[i], k);
+            cnt += map.getOrDefault(k / gcd, 0);
+            //更新num[i]的所有因子的出现次数
+            for (Integer div : divisors[nums[i]]) {
+                map.put(div, map.getOrDefault(div, 0) + 1);
+            }
+        }
+        return cnt;
+    }
+
+    private int gcd(int a, int b) {
+        if (a == 0) {
+            return 1;
+        }
+        return b == 0 ? a : gcd(b, a % b);
     }
 }
