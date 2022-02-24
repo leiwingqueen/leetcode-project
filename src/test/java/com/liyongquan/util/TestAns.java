3fc0a7cfd6021169cfd5e21d883231d132501cdd
leiwingqueen <leiwingqueen@qq.com>
2022-02-24 15:04:42 +0800

    括号匹配问题

---
+++ src/test/java/com/liyongquan/util/TestAns.java
@@ -0,0 +1,23 @@
+package com.liyongquan.util;
+
+import java.util.Scanner;
+
+public class TestAns {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        int pre = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                pre++;
+            } else if (s.charAt(i) == ')') {
+                pre--;
+            }
+            if (pre < 0) {
+                System.out.println("No");
+                return;
+            }
+        }
+        System.out.println("Yes");
+    }
+}
