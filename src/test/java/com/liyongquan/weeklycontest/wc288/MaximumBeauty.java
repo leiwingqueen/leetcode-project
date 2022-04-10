27c0e835f8129346ecb232ec0fde853bd3b89c94
leiwingqueen <leiwingqueen@qq.com>
2022-04-10 17:19:15 +0800

    周赛最后一题

--- src/test/java/com/liyongquan/weeklycontest/wc288/MaximumBeauty.java
+++ src/test/java/com/liyongquan/weeklycontest/wc288/MaximumBeauty.java
@@ -1,52 +1,63 @@
 package com.liyongquan.weeklycontest.wc288;
 
 import java.util.Arrays;
-import java.util.PriorityQueue;
 
 public class MaximumBeauty {
     public long maximumBeauty(int[] flowers, long newFlowers, int target, int full, int partial) {
         Arrays.sort(flowers);
-        int mx = 0;
-        long left = newFlowers;
-        for (int i = flowers.length - 1; i >= 0; i--) {
-            if (left <= 0) {
-                break;
+        int len = flowers.length;
+        if (flowers[0] >= target) {
+            return (long) full * len;
+        }
+        //找到最后一个非完善的花园的下标
+        int l = 0, r = len - 1;
+        while (l < r) {
+            int mid = l + (r - l + 1) / 2;
+            if (flowers[mid] < target) {
+                l = mid;
+            } else {
+                r = mid - 1;
             }
-            if (flowers[i] < target) {
-                long d = Math.min(left, target - flowers[i]);
-                if (flowers[i] + d >= target) {
-                    mx++;
-                    left -= d;
+        }
+        int idx = l + 1;
+        //[0,idx)是需要重新计算的部分，[idx,len)原本已经满足条件的完善花园，不需要计算
+        //遍历所有完善花园的数量
+        long res = 0;
+        //不完善花园的总数
+        long sum = 0;
+        for (int i = 0; i < idx; i++) {
+            sum += flowers[i];
+        }
+        for (int i = 0; i <= idx; i++) {
+            int p = i * full;
+            //不完善花园的数量
+            int q = idx - i;
+            //二分查找找到不完善花园的最大值
+            l = 1;
+            r = target - 1;
+            while (l < r) {
+                int mid = l + (r - l + 1) / 2;
+                if ((long) mid * q - sum <= newFlowers) {
+                    l = mid;
                 } else {
+                    r = mid - 1;
+                }
+            }
+            if (q > 0) {
+                p += l * partial;
+            }
+            res = Math.max(res, p);
+            //更新下一轮迭代
+            if (i < idx) {
+                int next = flowers[idx - i - 1];
+                if (newFlowers < target - next) {
                     break;
                 }
-            } else {
-                mx++;
+                sum -= next;
+                newFlowers -= target - next;
             }
         }
-        if (mx == flowers.length) {
-            return Math.max(full * mx, full * (mx - 1) + (target - 1) * partial);
-        }
-        if (left <= 0) {
-            return full * mx + flowers[0] * partial;
-        } else {
-            PriorityQueue<Integer> queue = new PriorityQueue<>();
-            for (int flower : flowers) {
-                queue.add(flower);
-            }
-            while (left > 0) {
-                Integer poll = queue.poll();
-                if (queue.size() == 0) {
-                    queue.add((int) left);
-                    left = 0;
-                }
-                Integer peek = queue.peek();
-                int n = Math.min((int) left, peek - poll + 1);
-                queue.add(poll + n);
-                left -= n;
-            }
-            return full * mx + queue.peek() * partial;
-        }
+        return res + (len - idx) * full;
     }
 
 }
--- src/test/java/com/liyongquan/weeklycontest/wc288/MaximumBeautyTest.java
+++ src/test/java/com/liyongquan/weeklycontest/wc288/MaximumBeautyTest.java
@@ -20,4 +20,15 @@ public class MaximumBeautyTest {
         long res = mb.maximumBeauty(new int[]{1, 3, 1, 1}, 7, 6, 12, 1);
         Assert.assertEquals(14, res);
     }
+
+    //[2,4,5,3]
+    //10
+    //5
+    //2
+    //6
+    @Test
+    public void test1() {
+        long res = mb.maximumBeauty(new int[]{2, 4, 5, 3}, 10, 5, 2, 6);
+        Assert.assertEquals(30, res);
+    }
 }
