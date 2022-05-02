d6033998f75655609d81c5cbb37beeb9b6599323
leiwingqueen <leiwingqueen@qq.com>
2022-05-02 21:09:58 +0800

    每日一题

---
+++ src/test/java/com/liyongquan/design/TagValidatorTest.java
@@ -0,0 +1,25 @@
+package com.liyongquan.design;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@Slf4j
+public class TagValidatorTest {
+    private TagValidator validator = new TagValidator();
+
+    @Test
+    public void isValid() {
+        boolean res = validator.isValid("<A>  <B> </A>   </B>");
+        Assert.assertEquals(false, res);
+    }
+
+    //"<DIV>This is the first line <![CDATA[<div>]]></DIV>"
+    @Test
+    public void test() {
+        boolean res = validator.isValid("<DIV>This is the first line <![CDATA[<div>]]></DIV>");
+        Assert.assertEquals(true, res);
+    }
+}
