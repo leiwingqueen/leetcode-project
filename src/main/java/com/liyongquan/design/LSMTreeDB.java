087477f80fa5dcb78a86c993488618807b64ad30
leiwingqueen <leiwingqueen@qq.com>
2022-05-06 09:35:36 +0800

    每日一题

--- src/main/java/com/liyongquan/design/LSMTreeDB.java
+++ src/main/java/com/liyongquan/design/LSMTreeDB.java
@@ -1,8 +1,14 @@
 package com.liyongquan.design;
 
+import com.alibaba.fastjson.JSON;
 import javafx.util.Pair;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.Collection;
 import java.util.Optional;
 import java.util.TreeMap;
@@ -16,6 +22,7 @@ public class LSMTreeDB {
     private static final String OP_RM = "RM";
 
     private TreeMap<String, Command> memTable;
+    private SSTable ssTable;
 
     public void put(String key, String value) throws IOException {
         memTable.put(key, new Command(OP_PUT, key, value));
@@ -34,6 +41,61 @@ public class LSMTreeDB {
 
     public Collection<Pair<String, String>> scan(String left, String right) {
         return null;
+    }
+
+    private static class SSTable {
+        public static final int BUFFER_SIZE = 1024;
+        private int segmentId;
+        private int partSize;
+        private String path;
+
+        public SSTable(int segmentId, int partSize, String path) {
+            this.segmentId = segmentId;
+            this.partSize = partSize;
+            this.path = path;
+        }
+
+        /**
+         * 持久化memTable到Segment
+         *
+         * @param memTable
+         */
+        public void persistent(TreeMap<String, Command> memTable) throws IOException {
+            ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
+            segmentId++;
+            String filename = buildFilename(path, String.valueOf(segmentId));
+            FileChannel fileChannel = new FileOutputStream(filename).getChannel();
+            for (Command command : memTable.values()) {
+                byte[] json = JSON.toJSONBytes(command);
+                buffer.putInt(json.length);
+                buffer.put(json);
+                if (buffer.position() >= partSize) {
+                    //写入一个索引
+                    fileChannel.write(buffer);
+                }
+                buffer.clear();
+            }
+            fileChannel.close();
+        }
+    }
+
+
+    private static String buildFilename(String path, String filename) {
+        StringBuilder sb = new StringBuilder(path);
+        if (path.charAt(path.length() - 1) != '/') {
+            sb.append('/');
+        }
+        sb.append(filename + ".log");
+        return sb.toString();
+    }
+
+    private static class SparseIndex {
+        TreeMap<String, SparseIndexItem> map;
+    }
+
+    private static class SparseIndexItem {
+        long offset;
+        int len;
     }
 
     private static class Command {
