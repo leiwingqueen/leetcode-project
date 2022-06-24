c3c4400edd4ff1e117a5f295c27ee960f151d2df
leiwingqueen <leiwingqueen@gmail.com>
2022-06-24 23:21:39 +0800

    目录结构修改

--- src/main/java/com/liyongquan/util/IBinarySearch.java
+++
@@ -1,8 +0,0 @@
-package com.liyongquan.util;
-
-/**
- * 二分查找
- */
-public interface IBinarySearch {
-    int search(int[] nums, int target);
-}
--- src/main/java/com/liyongquan/weeklycontest/bwc49/SquareIsWhite.java
+++
@@ -1,9 +0,0 @@
-package com.liyongquan.weeklycontest.bwc49;
-
-public class SquareIsWhite {
-    public boolean squareIsWhite(String coordinates) {
-        int idx1 = (coordinates.charAt(0) - 'a') & 0b1;
-        int idx2 = (coordinates.charAt(1) - 1) & 0b1;
-        return (idx1 ^ idx2) == 1;
-    }
-}
