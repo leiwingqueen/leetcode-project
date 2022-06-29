91675912cf32c4523b4a5be6784ad733aa86d32d
leiwingqueen <leiwingqueen@gmail.com>
2022-06-29 10:38:15 +0800

    c++语法

--- algorithm/cpp/src/main.cpp
+++ algorithm/cpp/src/main.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
+
+using namespace std;
+
+struct TestObj {
+    int value;
+
+    TestObj(int value) : value(value) {}
+};
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    for (int i = 0; i < 10; ++i) {
+        cout << i << endl;
+    }
+    //for 循环测试
+    cout << "====================================" << endl;
+    int arr[] = {1, 2, 3};
+    for (const auto &item: arr) {
+        cout << item << endl;
+    }
+    cout << "====================================" << endl;
+    for (int i: arr) {
+        cout << i << endl;
+        i = 3;
+    }
+    cout << "====================================" << endl;
+    for (int &i: arr) {
+        cout << i << endl;
+        i = 3;
+    }
+    cout << "====================================" << endl;
+    for (int i: arr) {
+        cout << i << endl;
+    }
+    cout << "====================================" << endl;
+    TestObj *arr2[] = {new TestObj(1), new TestObj(2)};
+    for (TestObj *obj: arr2) {
+        cout << obj->value << endl;
+    }
     return 0;
 }
