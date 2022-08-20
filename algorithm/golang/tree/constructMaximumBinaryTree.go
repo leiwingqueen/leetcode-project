8ff1565bd5f425167e65cb057ab3f6f834aba148
leiwingqueen <leiwingqueen@gmail.com>
2022-08-20 11:47:12 +0800

    每日一题

---
+++ algorithm/golang/tree/constructMaximumBinaryTree.go
@@ -0,0 +1,72 @@
+package tree
+
+//给定一个不重复的整数数组 nums 。 最大二叉树 可以用下面的算法从 nums 递归地构建:
+//
+//创建一个根节点，其值为 nums 中的最大值。
+//递归地在最大值 左边 的 子数组前缀上 构建左子树。
+//递归地在最大值 右边 的 子数组后缀上 构建右子树。
+//返回 nums 构建的 最大二叉树 。
+//
+//
+//
+//示例 1：
+//
+//
+//输入：nums = [3,2,1,6,0,5]
+//输出：[6,3,5,null,2,0,null,null,1]
+//解释：递归调用如下所示：
+//- [3,2,1,6,0,5] 中的最大值是 6 ，左边部分是 [3,2,1] ，右边部分是 [0,5] 。
+//    - [3,2,1] 中的最大值是 3 ，左边部分是 [] ，右边部分是 [2,1] 。
+//        - 空数组，无子节点。
+//        - [2,1] 中的最大值是 2 ，左边部分是 [] ，右边部分是 [1] 。
+//            - 空数组，无子节点。
+//            - 只有一个元素，所以子节点是一个值为 1 的节点。
+//    - [0,5] 中的最大值是 5 ，左边部分是 [0] ，右边部分是 [] 。
+//        - 只有一个元素，所以子节点是一个值为 0 的节点。
+//        - 空数组，无子节点。
+//示例 2：
+//
+//
+//输入：nums = [3,2,1]
+//输出：[3,null,2,null,1]
+//
+//
+//提示：
+//
+//1 <= nums.length <= 1000
+//0 <= nums[i] <= 1000
+//nums 中的所有整数 互不相同
+//
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode.cn/problems/maximum-binary-tree
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+// 解法1，顺序扫描得到每个区间的最大值
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+	var dfs func(l int, r int) *TreeNode
+	dfs = func(l int, r int) *TreeNode {
+		if l > r {
+			return nil
+		}
+		if l == r {
+			return &TreeNode{Val: nums[l]}
+		}
+		// find the biggest num
+		idx := 0
+		mx := -1
+		for i := l; i <= r; i++ {
+			if nums[i] > mx {
+				mx = nums[i]
+				idx = i
+			}
+		}
+		root := &TreeNode{Val: nums[idx]}
+		root.Left = dfs(l, idx-1)
+		root.Right = dfs(idx+1, r)
+		return root
+	}
+	return dfs(0, len(nums)-1)
+}
+
+// TODO:单调栈解法
