2c56561d942668be959eb31835e80478468d7ad3
leiwingqueen <leiwingqueen@gmail.com>
2022-09-08 15:29:35 +0800

    每日一题

--- algorithm/golang/weekcontest/bwc86/maximumRobots.go
+++ algorithm/golang/weekcontest/bwc86/maximumRobots.go
@@ -6,11 +6,36 @@ func maximumRobots(chargeTimes []int, runningCosts []int, budget int64) int {
 	stack := make([]int, 0)
 	l := 0
 	r := 0
-	sum := 0
-	for r < n {
-		if len(stack) == 0 || stack[len(stack)-1] <= chargeTimes[r] {
-			stack = append(stack, chargeTimes[r])
+	var sum int64
+	cal := func() int64 {
+		if l == r {
+			return 0
 		}
-		sum += chargeTimes[r]
+		return int64(chargeTimes[stack[0]]) + int64(r-l)*sum
 	}
+	res := 0
+	for r < n {
+		c := cal()
+		if c <= budget {
+			sum += int64(runningCosts[r])
+			for len(stack) > 0 && stack[len(stack)-1] < chargeTimes[r] {
+				stack = stack[:len(stack)-1]
+			}
+			stack = append(stack, r)
+			r++
+		} else {
+			if r-l > res {
+				res = r - l
+			}
+			sum -= int64(runningCosts[l])
+			if len(stack) > 0 && stack[0] == l {
+				stack = stack[1:]
+			}
+			l++
+		}
+	}
+	if cal() <= budget && r-l > res {
+		res = r - l
+	}
+	return res
 }
