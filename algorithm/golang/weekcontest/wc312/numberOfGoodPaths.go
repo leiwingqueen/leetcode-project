e9fa9656c2343095c824ab90df27972967af5085
leiwingqueen <leiwingqueen@gmail.com>
2022-09-25 21:14:32 +0800

    312场周赛

--- algorithm/golang/weekcontest/wc312/numberOfGoodPaths.go
+++ algorithm/golang/weekcontest/wc312/numberOfGoodPaths.go
@@ -1,5 +1,8 @@
 package wc312
 
+import "sort"
+
+// 超时
 func numberOfGoodPaths(vals []int, edges [][]int) int {
 	n := len(vals)
 	graph := make([][]int, n)
@@ -32,4 +35,53 @@ func numberOfGoodPaths(vals []int, edges [][]int) int {
 		res += dfs(i, i, visit)
 	}
 	return res/2 + n
+}
+
+// 并查集
+func numberOfGoodPaths2(vals []int, edges [][]int) int {
+	n := len(vals)
+	graph := make([][]int, n)
+	for i := 0; i < n; i++ {
+		graph[i] = make([]int, 0)
+	}
+	// 根据值进行排序
+	ids := make([]int, n)
+	for i := 0; i < n; i++ {
+		ids[i] = i
+	}
+	sort.Slice(ids, func(i, j int) bool {
+		return vals[ids[i]] < vals[ids[j]]
+	})
+	// 并查集
+	parent := make([]int, n)
+	cnt := make([]int, n)
+	for i := 0; i < n; i++ {
+		parent[i] = i
+		cnt[i] = 1
+	}
+	var find func(x int) int
+	find = func(x int) int {
+		for parent[x] != x {
+			parent[x] = parent[parent[x]]
+		}
+		return parent[x]
+	}
+	res := 0
+	for _, id := range ids {
+		vx := vals[id]
+		p := find(id)
+		// 判断是否要加入该边
+		for _, next := range graph[id] {
+			p2 := find(next)
+			if vals[p2] > vx || parent[p2] == p {
+				continue
+			}
+			if vals[p2] == vx {
+				res += cnt[p2] * cnt[p]
+				cnt[p] += cnt[p2]
+			}
+			parent[p2] = id
+		}
+	}
+	return res
 }
