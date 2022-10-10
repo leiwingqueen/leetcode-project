e18b05ac9b6491ceb1a39bc41572db6def76765f
leiwingqueen <leiwingqueen@gmail.com>
2022-10-10 14:47:57 +0800

    最小交换

---
+++ algorithm/golang/dp/minSwap.go
@@ -0,0 +1,63 @@
+package dp
+
+import (
+	"leetcode-go/util"
+	"math"
+)
+
+// 我们有两个长度相等且不为空的整型数组 nums1 和 nums2 。在一次操作中，我们可以交换 nums1[i] 和 nums2[i]的元素。
+//
+//例如，如果 nums1 = [1,2,3,8] ， nums2 =[5,6,7,4] ，你可以交换 i = 3 处的元素，得到 nums1 =[1,2,3,4] 和 nums2 =[5,6,7,8] 。
+//返回 使 nums1 和 nums2 严格递增 所需操作的最小次数 。
+//
+//数组 arr 严格递增 且  arr[0] < arr[1] < arr[2] < ... < arr[arr.length - 1] 。
+//
+//注意：
+//
+//用例保证可以实现操作。
+//
+//
+//示例 1:
+//
+//输入: nums1 = [1,3,5,4], nums2 = [1,2,3,7]
+//输出: 1
+//解释:
+//交换 A[3] 和 B[3] 后，两个数组如下:
+//A = [1, 3, 5, 7] ， B = [1, 2, 3, 4]
+//两个数组均为严格递增的。
+//示例 2:
+//
+//输入: nums1 = [0,3,5,8,9], nums2 = [2,1,4,6,9]
+//输出: 1
+//
+//
+//提示:
+//
+//2 <= nums1.length <= 105
+//nums2.length == nums1.length
+//0 <= nums1[i], nums2[i] <= 2 * 105
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode.cn/problems/minimum-swaps-to-make-sequences-increasing
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+func minSwap(nums1 []int, nums2 []int) int {
+	n := len(nums1)
+	dp0 := make([]int, n)
+	dp1 := make([]int, n)
+	dp0[0] = 0
+	dp1[0] = 1
+	for i := 1; i < n; i++ {
+		dp0[i] = math.MaxInt32
+		dp1[i] = math.MaxInt32
+		if nums1[i-1] < nums1[i] && nums2[i-1] < nums2[i] {
+			dp0[i] = dp0[i-1]
+			dp1[i] = dp1[i-1] + 1
+		}
+		if nums1[i-1] < nums2[i] && nums2[i-1] < nums1[i] {
+			dp0[i] = util.Min(dp0[i], dp1[i-1])
+			dp1[i] = util.Min(dp1[i], dp0[i-1]+1)
+		}
+	}
+	return util.Min(dp0[n-1], dp1[n-1])
+}
