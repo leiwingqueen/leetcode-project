6634b371190f734ce2f7f75c42d53ff02313122f
leiwingqueen <leiwingqueen@gmail.com>
2022-11-08 19:56:19 +0800

    周赛最后一题

--- algorithm/golang/weekcontest/wc318/minimumTotalDistance.go
+++ algorithm/golang/weekcontest/wc318/minimumTotalDistance.go
@@ -1,6 +1,10 @@
 package wc318
 
-import "leetcode-go/util"
+import (
+	"leetcode-go/util"
+	"math"
+	"sort"
+)
 
 // 贪心算法，不通过
 func minimumTotalDistance(robot []int, factory [][]int) int64 {
@@ -46,4 +50,47 @@ func minimumTotalDistance(robot []int, factory [][]int) int64 {
 	return res
 }
 
-// 其实问题相当于n个物品，m个背包
+func minimumTotalDistance2(robot []int, factory [][]int) int64 {
+	sort.Ints(robot)
+	sort.Slice(factory, func(i, j int) bool {
+		return factory[i][0] <= factory[j][0]
+	})
+	m := len(robot)
+	n := len(factory)
+	dp := make([][]int64, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int64, m)
+	}
+	var sum int64
+	for i := m - 1; i >= 0; i-- {
+		sum += int64(util.Abs(robot[i] - factory[n-1][0]))
+		if m-i <= factory[n-1][1] {
+			dp[n-1][i] = sum
+		} else {
+			dp[n-1][i] = -1
+		}
+	}
+	var cost func(i int, j int, k int) int64
+	cost = func(i int, j int, k int) int64 {
+		var res int64
+		for l := 0; l < k; l++ {
+			res += int64(util.Abs(robot[j+l] - factory[i][0]))
+		}
+		return res
+	}
+	for i := n - 2; i >= 0; i-- {
+		for j := m - 1; j >= 0; j-- {
+			var min int64 = math.MaxInt64
+			for k := 0; j+k < m && k <= factory[i][1]; k++ {
+				if dp[i+1][j+k] > 0 {
+					sub := dp[i+1][j+k] + cost(i, j, k)
+					if sub < min {
+						min = sub
+					}
+				}
+			}
+			dp[i][j] = min
+		}
+	}
+	return dp[0][0]
+}
