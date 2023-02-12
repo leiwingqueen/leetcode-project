e0e406e908353c841ef1cfe2c98b23ecf5791336
leiwingqueen <leiwingqueen@gmail.com>
2023-02-12 16:35:37 +0800

    周赛

--- algorithm/golang/weekcontest/wc332/countFairPairs.go
+++ algorithm/golang/weekcontest/wc332/countFairPairs.go
@@ -2,23 +2,54 @@ package wc332
 
 import "sort"
 
+// 给你一个下标从 0 开始、长度为 n 的整数数组 nums ，和两个整数 lower 和 upper ，返回 公平数对的数目 。
+//
+//如果 (i, j) 数对满足以下情况，则认为它是一个 公平数对 ：
+//
+//0 <= i < j < n，且
+//lower <= nums[i] + nums[j] <= upper
+//
+//
+//示例 1：
+//
+//输入：nums = [0,1,7,4,4,5], lower = 3, upper = 6
+//输出：6
+//解释：共计 6 个公平数对：(0,3)、(0,4)、(0,5)、(1,3)、(1,4) 和 (1,5) 。
+//示例 2：
+//
+//输入：nums = [1,7,9,2,5], lower = 11, upper = 11
+//输出：1
+//解释：只有单个公平数对：(2,3) 。
+//
+//
+//提示：
+//
+//1 <= nums.length <= 105
+//nums.length == n
+//-109 <= nums[i] <= 109
+//-109 <= lower <= upper <= 109
+//
+//来源：力扣（LeetCode）
+//链接：https://leetcode.cn/problems/count-the-number-of-fair-pairs
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
 func countFairPairs(nums []int, lower int, upper int) int64 {
 	n := len(nums)
 	sort.Ints(nums)
 	cal := func(mx int) int64 {
 		l := 0
-		r := 1
+		r := n - 1
 		var res int64
-		for r < n {
-			if nums[r]+nums[l] <= mx {
-				r++
-			} else {
-				res += int64(r - l - 1)
-				l++
-				r = l + 1
+		for l < r {
+			for l < r && nums[l]+nums[r] > mx {
+				r--
 			}
+			if l == r {
+				break
+			}
+			res += int64(r - l)
+			l++
 		}
-		res += int64(r - l - 1)
 		return res
 	}
 	return cal(upper) - cal(lower-1)
--- algorithm/golang/weekcontest/wc332/substringXorQueries.go
+++ algorithm/golang/weekcontest/wc332/substringXorQueries.go
@@ -43,3 +43,34 @@ func substringXorQueries(s string, queries [][]int) [][]int {
 	}
 	return res
 }
+
+func substringXorQueries2(s string, queries [][]int) [][]int {
+	mxLen := 30
+	n := len(s)
+	mp := make(map[int][]int)
+	for i := 1; i <= mxLen; i++ {
+		for j := 0; j <= n-i; j++ {
+			num := 0
+			// TODO:这里还是会有一些重复计算
+			for l := 0; l < i; l++ {
+				num <<= 1
+				if s[j+l] == '1' {
+					num += 1
+				}
+			}
+			if _, exist := mp[num]; !exist {
+				mp[num] = []int{j, j + i - 1}
+			}
+		}
+	}
+	res := make([][]int, len(queries))
+	for i, query := range queries {
+		val := query[0] ^ query[1]
+		if r, exist := mp[val]; exist {
+			res[i] = r
+		} else {
+			res[i] = []int{-1, -1}
+		}
+	}
+	return res
+}
