9467bde6c09e402b665dc734d2495a1f2fc835a8
leiwingqueen <leiwingqueen@gmail.com>
2023-05-30 14:23:22 +0800

    105场双周赛

--- algorithm/golang/weekcontest/bwc105/minExtraChar.go
+++ algorithm/golang/weekcontest/bwc105/minExtraChar.go
@@ -1,5 +1,33 @@
 package bwc105
 
+import "strings"
+
+// 最傻的回溯，超时
 func minExtraChar(s string, dictionary []string) int {
-	return 0
+	dictMap := make(map[string]struct{})
+	for _, d := range dictionary {
+		dictMap[d] = struct{}{}
+	}
+	var dfs func(str string) int
+	dfs = func(str string) int {
+		res := len(str)
+		for d := range dictMap {
+			idx := strings.Index(str, d)
+			if idx >= 0 {
+				// 分成两部分[0,idx)和[idx+len(d),n)
+				sub := 0
+				if idx > 0 {
+					sub += dfs(str[:idx])
+				}
+				if idx+len(d) < len(str) {
+					sub += dfs(str[idx+len(d):])
+				}
+				if sub < res {
+					res = sub
+				}
+			}
+		}
+		return res
+	}
+	return dfs(s)
 }
