2471daadefd4adbe42e31b87fa0c071750be20d8
leiwingqueen <leiwingqueen@gmail.com>
2023-06-04 16:07:04 +0800

    工作计划

--- algorithm/golang/weekcontest/wc348/matrixSumQueries.go
+++ algorithm/golang/weekcontest/wc348/matrixSumQueries.go
@@ -1,5 +1,6 @@
 package wc348
 
+// 模拟，必然超时
 func matrixSumQueries(n int, queries [][]int) int64 {
 	matrix := make([][]int, n)
 	for i := 0; i < n; i++ {
@@ -24,4 +25,18 @@ func matrixSumQueries(n int, queries [][]int) int64 {
 		}
 	}
 	return res
+}
+
+func matrixSumQueries2(n int, queries [][]int) int64 {
+	// 记录每一行对应的行和列的最后修改
+	rows, cols := make([]int, n), make([]int, n)
+	for _, query := range queries {
+		t, index, v := query[0], query[1], query[2]
+		if t == 0 {
+			rows[index] = v
+		} else {
+			cols[index] = v
+		}
+	}
+	return 0
 }
