2c6b3b965f8a108ea16e2e9057c183212cd4e74d
leiwingqueen <leiwingqueen@gmail.com>
2023-06-19 00:13:34 +0800

    350场周赛

--- algorithm/golang/weekcontest/wc350/specialPerm.go
+++ algorithm/golang/weekcontest/wc350/specialPerm.go
@@ -1,5 +1,6 @@
 package wc350
 
+// 超时
 func specialPerm(nums []int) int {
 	n := len(nums)
 	mod := 1_000_000_007
@@ -20,4 +21,30 @@ func specialPerm(nums []int) int {
 		return res
 	}
 	return dfs(nums, 0)
+}
+
+// 状态压缩，记忆
+func specialPerm2(nums []int) int {
+	n := len(nums)
+	mod := 1_000_000_007
+	// mem := make(map[int]int)
+	var dfs func(state int, pre int) int
+	dfs = func(state int, pre int) int {
+		if state == 0 {
+			return 1
+		}
+		/*if v, exist := mem[state]; exist {
+			return v
+		}*/
+		res := 0
+		for i := 0; i < n; i++ {
+			if state&(1<<i) != 0 && (nums[i]%pre == 0 || pre%nums[i] == 0) {
+				res = (res + dfs(state^(1<<i), nums[i])) % mod
+			}
+		}
+		// mem[state] = res
+		// fmt.Println(fmt.Sprintf("state:%d,pre:%d,res:%d", state, pre, res))
+		return res
+	}
+	return dfs((1<<n)-1, 1)
 }
