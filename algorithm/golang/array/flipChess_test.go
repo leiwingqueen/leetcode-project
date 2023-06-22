d8bc3246807d2c03ac7f19cd6d8ee40e63a3828a
leiwingqueen <leiwingqueen@gmail.com>
2023-06-22 22:21:56 +0800

    每日一题

---
+++ algorithm/golang/array/flipChess_test.go
@@ -0,0 +1,24 @@
+package array
+
+import "testing"
+
+func Test_flipChess(t *testing.T) {
+	type args struct {
+		chessboard []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"t1", args{chessboard: []string{"....X.", "....X.", "XOOO..", "......", "......"}}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := flipChess(tt.args.chessboard); got != tt.want {
+				t.Errorf("flipChess() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
