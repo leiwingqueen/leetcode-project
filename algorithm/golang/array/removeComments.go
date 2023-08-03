b017f5562515d0560cffb49adb1c89500f1b889c
leiwingqueen <leiwingqueen@gmail.com>
2023-08-03 21:57:26 +0800

    每日一题

--- algorithm/golang/array/removeComments.go
+++ algorithm/golang/array/removeComments.go
@@ -4,37 +4,36 @@ import "strings"
 
 func removeComments(source []string) []string {
 	var res []string
-	i := 0
-	for i < len(source) {
-		s := source[i]
-		idx1 := strings.Index(s, "//")
-		idx2 := strings.Index(s, "/*")
-		if idx1 >= 0 && idx2 >= 0 {
-			if idx1 < idx2 {
-				s = s[0:idx1]
+	block := false
+	builder := strings.Builder{}
+	for _, s := range source {
+		i := 0
+		n := len(s)
+		for i < n {
+			ch := s[i]
+			if block {
+				if ch == '*' && (i < n-1 && s[i+1] == '/') {
+					block = false
+					i += 2
+				} else {
+					i++
+				}
 			} else {
-				s = s[0:idx2]
+				if ch == '/' && (i < n-1 && s[i+1] == '/') {
+					break
+				} else if ch == '/' && (i < n-1 && s[i+1] == '*') {
+					// 看下后面是否存在*/
+					block = true
+					i += 2
+				} else {
+					builder.WriteByte(s[i])
+					i++
+				}
 			}
-		} else if idx1 >= 0 {
-			s = s[0:idx1]
-		} else if idx2 >= 0 {
-			s = s[0:idx2]
 		}
-		if s != "" {
-			res = append(res, s)
-		}
-		
-		if strings.HasPrefix(s, "//") {
-			i++
-		} else if strings.HasPrefix(s, "/*") {
-			p := i
-			for p < len(source) && !strings.HasSuffix(source[p], "*/") {
-				p++
-			}
-			i = p + 1
-		} else {
-			res = append(res, source[i])
-			i++
+		if builder.Len() > 0 && !block {
+			res = append(res, builder.String())
+			builder.Reset()
 		}
 	}
 	return res
