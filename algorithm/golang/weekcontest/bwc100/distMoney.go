b9eba213f3ccf61e667e4cc795ce79c792cdeca0
leiwingqueen <leiwingqueen@gmail.com>
2023-09-22 12:18:06 +0800

    每日一题

--- algorithm/golang/weekcontest/bwc100/distMoney.go
+++ algorithm/golang/weekcontest/bwc100/distMoney.go
@@ -23,3 +23,34 @@ func distMoney(money int, children int) int {
 	}
 	return children
 }
+
+func distMoney2(money int, children int) int {
+	n := children
+	k := money
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, k+1)
+	}
+	dp[0][4] = -1
+	dp[0][8] = 1
+	for i := 0; i < n; i++ {
+		dp[i][0] = -1
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j <= k; j++ {
+			dp[i][j] = -1
+			for l := 1; l < j; l++ {
+				if dp[i-1][j-l] >= 0 && l != 4 {
+					c := 0
+					if l == 8 {
+						c = 1
+					}
+					if dp[i][j] < 0 || dp[i-1][j-l]+c > dp[i][j] {
+						dp[i][j] = -1
+					}
+				}
+			}
+		}
+	}
+	return dp[n-1][k]
+}
