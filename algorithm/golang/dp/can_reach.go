49cd8f4f9e33cf8b39df4deecfc760d3ed57543c
leiwingqueen <leiwingqueen@gmail.com>
2023-09-28 15:01:26 +0800

    每日一题

--- algorithm/golang/dp/can_reach.go
+++ algorithm/golang/dp/can_reach.go
@@ -1,5 +1,11 @@
 package dp
 
+// 微软面试题
+// kangaroo jump
+// arr表示长度为n的马路，其中1表示该坐标上有陷阱，0表示没有。
+// 袋鼠有两种跳法，往前调1格，或者往前跳minJump~maxJump格(假设当前下标是i,则范围是[i+minJump+1,i+maxJump+1])
+// 求袋鼠是否能到达最后一个点
+
 // f(n)=f(n-1)|| {f(n-maxJump-1)...f(n-minJump-1)}
 func canReach(arr []int, minJump int, maxJump int) bool {
 	max := func(a, b int) int {
@@ -39,4 +45,43 @@ func canReach(arr []int, minJump int, maxJump int) bool {
 		}
 	}
 	return dp[n-1]
+}
+
+// 上面基础上增加一个条件，假设袋鼠能往后跳，跳的范围跟上面一致
+
+// 假设能往后跳,bfs
+func canReach2(arr []int, minJump int, maxJump int) bool {
+	n := len(arr)
+	visit := make([]bool, n)
+	visit[0] = true
+	queue := []int{0}
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+		// 往前和后退一步
+		if node+1 < n && !visit[node+1] && arr[node+1] == 0 {
+			queue = append(queue, node+1)
+			visit[node+1] = true
+		}
+		if node-1 >= 0 && !visit[node-1] && arr[node-1] == 0 {
+			queue = append(queue, node-1)
+			visit[node-1] = true
+		}
+		// 往前和往后若干步
+		for i := minJump; i <= maxJump && node+i+1 < n; i++ {
+			next := node + i + 1
+			if !visit[next] && arr[next] == 0 {
+				queue = append(queue, next)
+				visit[next] = true
+			}
+		}
+		for i := minJump; i <= maxJump && node-i-1 >= 0; i++ {
+			next := node - i - 1
+			if !visit[next] && arr[next] == 0 {
+				queue = append(queue, next)
+				visit[next] = true
+			}
+		}
+	}
+	return visit[n-1]
 }
--- algorithm/golang/dp/can_reach_test.go
+++ algorithm/golang/dp/can_reach_test.go
@@ -16,15 +16,38 @@ func Test_canReach(t *testing.T) {
 		// TODO: Add test cases.
 		{"t1", args{[]int{0, 0, 1, 1, 0}, 2, 4}, true},
 		{"t2", args{[]int{0, 0, 1, 1, 1, 1, 0}, 2, 4}, true},
-		{"t3", args{[]int{0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0}, 2, 4}, false},
+		{"t3", args{[]int{0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0}, 2, 4}, false},
 		{"t4", args{[]int{0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0}, 2, 4}, true},
 		{"t5", args{[]int{0, 0, 0}, 2, 4}, true},
 		{"t6", args{[]int{0, 0, 0, 1, 1, 0}, 1, 1}, false},
+		{"t7", args{[]int{0, 1, 0, 0, 1, 0}, 2, 3}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := canReach(tt.args.arr, tt.args.minJump, tt.args.maxJump); got != tt.want {
 				t.Errorf("canReach() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_canReach2(t *testing.T) {
+	type args struct {
+		arr     []int
+		minJump int
+		maxJump int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{"t1", args{[]int{0, 1, 0, 0, 1, 0}, 2, 3}, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := canReach2(tt.args.arr, tt.args.minJump, tt.args.maxJump); got != tt.want {
+				t.Errorf("canReach2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
