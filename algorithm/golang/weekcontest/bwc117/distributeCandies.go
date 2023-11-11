63616e78012c2349a0c19beadd310644f3b7ad77
leiwingqueen <leiwingqueen@gmail.com>
2023-11-12 00:09:06 +0800

    双周赛

---
+++ algorithm/golang/weekcontest/bwc117/distributeCandies.go
@@ -0,0 +1,51 @@
+package bwc117
+
+func distributeCandies(n int, limit int) int {
+	var dfs func(k int, n int) int
+	dfs = func(k int, n int) int {
+		if k == 1 {
+			if n <= limit {
+				return 1
+			} else {
+				return 0
+			}
+		} else {
+			res := 0
+			for i := 0; i <= n && i <= limit; i++ {
+				res += dfs(k-1, n-i)
+			}
+			return res
+		}
+	}
+	return dfs(3, n)
+}
+
+// 优化解法
+func distributeCandies2(n int, limit int) int64 {
+	mem := make(map[int]int64)
+	var dfs func(k int, n int) int64
+	dfs = func(k int, n int) int64 {
+		if k == 1 {
+			if n <= limit {
+				return 1
+			} else {
+				return 0
+			}
+		} else {
+			if k == 2 {
+				if v, ok := mem[n]; ok {
+					return v
+				}
+			}
+			var res int64
+			for i := 0; i <= n && i <= limit; i++ {
+				res += dfs(k-1, n-i)
+			}
+			if k == 2 {
+				mem[n] = res
+			}
+			return res
+		}
+	}
+	return dfs(3, n)
+}
---
+++ algorithm/golang/weekcontest/bwc117/stringCount_test.go
@@ -0,0 +1,24 @@
+package bwc117
+
+import "testing"
+
+func Test_stringCount(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"t1", args{10}, 83943898},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := stringCount2(tt.args.n); got != tt.want {
+				t.Errorf("stringCount() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
