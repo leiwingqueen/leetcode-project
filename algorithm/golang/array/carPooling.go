bcd34972077b73aeed406862f24621263673f3c0
leiwingqueen <leiwingqueen@gmail.com>
2023-12-02 16:36:53 +0800

    每日一题

--- algorithm/golang/array/carPooling.go
+++ algorithm/golang/array/carPooling.go
@@ -1,5 +1,7 @@
 package array
 
+import "sort"
+
 // There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
 //
 //You are given the integer capacity and an array trips where trips[i] = [numPassengersi, fromi, toi] indicates that the ith trip has numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively. The locations are given as the number of kilometers due east from the car's initial location.
@@ -36,6 +38,27 @@ func carPooling(trips [][]int, capacity int) bool {
 			if cnt[i] > capacity {
 				return false
 			}
+		}
+	}
+	return true
+}
+
+// 差分数组
+func carPooling2(trips [][]int, capacity int) bool {
+	cnt := make([]int, 1001)
+	sort.Slice(trips, func(i, j int) bool {
+		return trips[i][1] < trips[j][1]
+	})
+	for _, trip := range trips {
+		c, from, to := trip[0], trip[1], trip[2]
+		cnt[from] += c
+		cnt[to] -= c
+	}
+	cur := 0
+	for _, c := range cnt {
+		cur += c
+		if cur > capacity {
+			return false
 		}
 	}
 	return true
