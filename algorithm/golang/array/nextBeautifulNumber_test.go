5fe2f3de1c945b29191972c310edc70a8b79ff78
leiwingqueen <leiwingqueen@gmail.com>
2023-12-09 11:07:32 +0800

    每日一题

---
+++ algorithm/golang/array/nextBeautifulNumber_test.go
@@ -0,0 +1,24 @@
+package array
+
+import "testing"
+
+func Test_nextBeautifulNumber(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"t1", args{1}, 22},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := nextBeautifulNumber(tt.args.n); got != tt.want {
+				t.Errorf("nextBeautifulNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
