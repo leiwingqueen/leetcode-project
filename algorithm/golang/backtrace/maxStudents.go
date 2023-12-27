22419f2e426d10ae495f89f6a28cbecb37f3c281
leiwingqueen <leiwingqueen@gmail.com>
2023-12-27 16:06:55 +0800

    每日一题

--- algorithm/golang/backtrace/maxStudents.go
+++ algorithm/golang/backtrace/maxStudents.go
@@ -45,14 +45,14 @@ package backtrace
 //1 <= m <= 8
 //1 <= n <= 8
 
-// 暴力回溯
+// 暴力回溯，先验证逻辑OK，超时
 func maxStudents(seats [][]byte) int {
 	m, n := len(seats), len(seats[0])
 	dirs := [][]int{
 		{0, -1},
 		{0, 1},
 		{-1, -1},
-		{1, 1},
+		{-1, 1},
 	}
 	var dfs func(state int64, x, y int) int
 	dfs = func(state int64, x, y int) int {
@@ -68,6 +68,58 @@ func maxStudents(seats [][]byte) int {
 		}
 		// 不坐人的场景
 		res := dfs(state, x, y+1)
+		// 先检查能不能坐人
+		flag := true
+		for _, dir := range dirs {
+			nx, ny := x+dir[0], y+dir[1]
+			if nx >= 0 && nx < m && ny >= 0 && ny < n && (state&(1<<(nx*n+ny))) != 0 {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			sub := dfs(state|(1<<(x*n+y)), x, y+1) + 1
+			if sub > res {
+				res = sub
+			}
+		}
+		return res
+	}
+	return dfs(0, 0, 0)
+}
+
+// 这样写还是会超时
+func maxStudents2(seats [][]byte) int {
+	m, n := len(seats), len(seats[0])
+	dirs := [][]int{
+		{0, -1},
+		{0, 1},
+		{-1, -1},
+		{-1, 1},
+	}
+	mem := make(map[int]map[int64]int)
+	var dfs func(state int64, x, y int) int
+	dfs = func(state int64, x, y int) int {
+		if y == n {
+			x++
+			y = 0
+		}
+		if x == m {
+			return 0
+		}
+		if _, ok := mem[x*n+y]; !ok {
+			mem[x*n+y] = make(map[int64]int)
+		}
+		if v, ok := mem[x*n+y][state]; ok {
+			return v
+		}
+		res := dfs(state, x, y+1)
+		defer func() {
+			mem[x*n+y][state] = res
+		}()
+		if seats[x][y] == '#' {
+			return res
+		}
 		// 先检查能不能坐人
 		flag := true
 		for _, dir := range dirs {
