f3670f4ff246bb914a7f5d11b228d3669f9bcab0
leiwingqueen <leiwingqueen@gmail.com>
2024-01-03 09:04:27 +0800

    每日一题

--- algorithm/golang/weekcontest/wc321/removeNodes.go
+++ algorithm/golang/weekcontest/wc321/removeNodes.go
@@ -1,5 +1,35 @@
 package wc321
 
+// 给你一个链表的头节点 head 。
+//
+//移除每个右侧有一个更大数值的节点。
+//
+//返回修改后链表的头节点 head 。
+//
+//
+//
+//示例 1：
+//
+//
+//
+//输入：head = [5,2,13,3,8]
+//输出：[13,8]
+//解释：需要移除的节点是 5 ，2 和 3 。
+//- 节点 13 在节点 5 右侧。
+//- 节点 13 在节点 2 右侧。
+//- 节点 8 在节点 3 右侧。
+//示例 2：
+//
+//输入：head = [1,1,1,1]
+//输出：[1,1,1,1]
+//解释：每个节点的值都是 1 ，所以没有需要移除的节点。
+//
+//
+//提示：
+//
+//给定列表中的节点数目在范围 [1, 105] 内
+//1 <= Node.val <= 105
+
 type ListNode struct {
 	Val  int
 	Next *ListNode
@@ -37,4 +67,25 @@ func removeNodes(head *ListNode) *ListNode {
 		idx++
 	}
 	return dummy.Next
+}
+
+func removeNodes2(head *ListNode) *ListNode {
+	var dfs func(node *ListNode) (int, *ListNode)
+	dfs = func(node *ListNode) (int, *ListNode) {
+		if node == nil {
+			return 0, nil
+		}
+		mx, h := dfs(node.Next)
+		if node.Val >= mx {
+			node.Next = h
+			mx = node.Val
+		} else {
+			// 需要删除节点
+			node.Next = nil
+			node = h
+		}
+		return mx, node
+	}
+	_, node := dfs(head)
+	return node
 }
