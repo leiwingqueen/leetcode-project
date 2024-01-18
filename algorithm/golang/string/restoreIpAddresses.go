5c7f1ecac0a7bfcf8535fc00befed4c6202f148a
leiwingqueen <leiwingqueen@gmail.com>
2024-01-18 12:05:05 +0800

    每日一题

---
+++ algorithm/golang/string/restoreIpAddresses.go
@@ -0,0 +1,56 @@
+package string
+
+// 有效 IP 地址 正好由四个整数（每个整数位于 0 到 255 之间组成，且不能含有前导 0），整数之间用 '.' 分隔。
+//
+//例如："0.1.2.201" 和 "192.168.1.1" 是 有效 IP 地址，但是 "0.011.255.245"、"192.168.1.312" 和 "192.168@1.1" 是 无效 IP 地址。
+//给定一个只包含数字的字符串 s ，用以表示一个 IP 地址，返回所有可能的有效 IP 地址，这些地址可以通过在 s 中插入 '.' 来形成。你 不能 重新排序或删除 s 中的任何数字。你可以按 任何 顺序返回答案。
+//
+//
+//
+//示例 1：
+//
+//输入：s = "25525511135"
+//输出：["255.255.11.135","255.255.111.35"]
+//示例 2：
+//
+//输入：s = "0000"
+//输出：["0.0.0.0"]
+//示例 3：
+//
+//输入：s = "101023"
+//输出：["1.0.10.23","1.0.102.3","10.1.0.23","10.10.2.3","101.0.2.3"]
+//
+//
+//提示：
+//
+//1 <= s.length <= 20
+//s 仅由数字组成
+
+func restoreIpAddresses(s string) []string {
+	n := len(s)
+	var res []string
+	var dfs func(idx1, idx2 int, path []byte)
+	dfs = func(idx1, idx2 int, path []byte) {
+		if idx1 == n {
+			res = append(res, string(path[:idx2-1]))
+			return
+		}
+		if s[idx1] == '0' {
+			path[idx2] = '0'
+			dfs(idx1+1, idx2+1, path)
+		} else {
+			num := 0
+			for i := 0; i < 3; i++ {
+				if idx1+i >= n || num > 255 {
+					break
+				}
+				path[idx2+i] = s[idx1+i]
+				path[idx2+i+1] = '.'
+				dfs(idx1+i+1, idx2+i+2, path)
+				num = num*10 + int(s[idx1+i]-'0')
+			}
+		}
+	}
+	dfs(0, 0, make([]byte, n+4))
+	return res
+}
---
+++ algorithm/golang/string/restoreIpAddresses_test.go
@@ -0,0 +1,27 @@
+package string
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_restoreIpAddresses(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		// TODO: Add test cases.
+		{"t1", args{"25525511135"}, []string{"255.255.11.135", "255.255.111.35"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := restoreIpAddresses(tt.args.s); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("restoreIpAddresses() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
