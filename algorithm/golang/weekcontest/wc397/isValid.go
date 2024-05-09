297cc71ef7f372ad8b480ee1b3dda56dfb4f7d64
leiwingqueen <leiwingqueen@gmail.com>
2024-05-09 22:38:16 +0800

    每日一题

---
+++ algorithm/golang/weekcontest/wc397/isValid.go
@@ -0,0 +1,31 @@
+package wc397
+
+func isValid(word string) bool {
+	mp := map[byte]bool{
+		'a': true,
+		'e': true,
+		'i': true,
+		'o': true,
+		'u': true,
+		'A': true,
+		'E': true,
+		'I': true,
+		'O': true,
+		'U': true,
+	}
+	c1, c2 := 0, 0
+	for _, ch := range word {
+		if ch >= '0' && ch <= '9' {
+
+		} else if ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' {
+			if mp[byte(ch)] {
+				c1++
+			} else {
+				c2++
+			}
+		} else {
+			return false
+		}
+	}
+	return c1 > 0 && c2 > 0 && len(word) >= 3
+}
---
+++ algorithm/golang/weekcontest/wc397/minimumOperationsToMakeKPeriodic.go
@@ -0,0 +1,14 @@
+package wc397
+
+func minimumOperationsToMakeKPeriodic(word string, k int) int {
+	n := len(word)
+	mp := make(map[string]int)
+	for i := 0; i < n; i += k {
+		mp[word[i:i+k]]++
+	}
+	mx := 0
+	for _, v := range mp {
+		mx = max(mx, v)
+	}
+	return n/k - mx
+}
