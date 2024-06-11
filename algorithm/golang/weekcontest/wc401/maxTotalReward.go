ff71ce92c6ab99f500c64d2b89ba4e045e335faa
leiwingqueen <leiwingqueen@gmail.com>
2024-06-11 19:36:47 +0700

    401场周赛题3

--- algorithm/golang/weekcontest/wc401/maxTotalReward.go
+++ algorithm/golang/weekcontest/wc401/maxTotalReward.go
@@ -33,3 +33,37 @@ func maxTotalReward2(rewardValues []int) int {
 	}
 	return dp[n]
 }
+
+// 假设f(n,x)为前n个数字的和<=x的最大值
+// f(n,x)=max(f(n-1,x),f(n-1,x-a[n-1]-1)+a[n-1])
+// 最终我们需要求的是f(n,s)，假设s为数组rewardValues的总和
+// 当n==0，值为0
+func maxTotalReward3(rewardValues []int) int {
+	sort.Ints(rewardValues)
+	n := len(rewardValues)
+	sum := 0
+	for _, v := range rewardValues {
+		sum += v
+	}
+	mx := max(sum, 2*rewardValues[n-1])
+	dp := make([][]int, n+1)
+	for i := 0; i <= n; i++ {
+		dp[i] = make([]int, mx+1)
+	}
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= mx; j++ {
+			dp[i][j] = dp[i-1][j]
+			// 找到最后一个下标k,k<rewardValues[i-1]
+			// 也就是k<rewardValues[i-1]
+			// 其中k<=j
+			if j-rewardValues[i-1] >= 0 {
+				if j-rewardValues[i-1] < rewardValues[i-1] {
+					dp[i][j] = max(dp[i][j], dp[i-1][j-rewardValues[i-1]]+rewardValues[i-1])
+				} else {
+					dp[i][j] = max(dp[i][j], dp[i-1][rewardValues[i-1]-1]+rewardValues[i-1])
+				}
+			}
+		}
+	}
+	return dp[n][mx]
+}
--- algorithm/golang/weekcontest/wc401/maxTotalReward_test.go
+++ algorithm/golang/weekcontest/wc401/maxTotalReward_test.go
@@ -16,7 +16,7 @@ func Test_maxTotalReward2(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := maxTotalReward2(tt.args.rewardValues); got != tt.want {
+			if got := maxTotalReward3(tt.args.rewardValues); got != tt.want {
 				t.Errorf("maxTotalReward2() = %v, want %v", got, tt.want)
 			}
 		})
