a341ea2746660e608a174606548dd4931766ce0f
leiwingqueen <leiwingqueen@gmail.com>
2024-06-13 23:14:25 +0800

    周赛

--- algorithm/golang/weekcontest/wc401/maxTotalReward.go
+++ algorithm/golang/weekcontest/wc401/maxTotalReward.go
@@ -1,6 +1,10 @@
 package wc401
 
-import "sort"
+import (
+	"math/big"
+	"slices"
+	"sort"
+)
 
 // 贪心
 func maxTotalReward(rewardValues []int) int {
@@ -68,7 +72,7 @@ func maxTotalReward3(rewardValues []int) int {
 	return dp[n][mx]
 }
 
-// 空间优化
+// 空间优化，还是超时
 func maxTotalReward4(rewardValues []int) int {
 	sort.Ints(rewardValues)
 	n := len(rewardValues)
@@ -96,4 +100,44 @@ func maxTotalReward4(rewardValues []int) int {
 		copy(pre, dp)
 	}
 	return dp[mx]
+}
+
+// 最大值为2*rewardValues[n-1]
+func maxTotalReward5(rewardValues []int) int {
+	sort.Ints(rewardValues)
+	n := len(rewardValues)
+	mx := 2 * rewardValues[n-1]
+	pre := make([]int, mx+1)
+	dp := make([]int, mx+1)
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= mx; j++ {
+			dp[j] = pre[j]
+			// 找到最后一个下标k,k<rewardValues[i-1]
+			// 也就是k<rewardValues[i-1]
+			// 其中k<=j
+			if j-rewardValues[i-1] >= 0 {
+				if j-rewardValues[i-1] < rewardValues[i-1] {
+					dp[j] = max(dp[j], pre[j-rewardValues[i-1]]+rewardValues[i-1])
+				} else {
+					dp[j] = max(dp[j], pre[rewardValues[i-1]-1]+rewardValues[i-1])
+				}
+			}
+		}
+		copy(pre, dp)
+	}
+	return dp[mx]
+}
+
+func maxTotalReward6(rewardValues []int) int {
+	sort.Ints(rewardValues)
+	rewardValues = slices.Compact(rewardValues)
+	p1 := big.NewInt(1)
+	for _, v := range rewardValues {
+		mask := big.NewInt(0)
+		mask = mask.Lsh(p1, uint(v))
+		p2 := big.NewInt(0)
+		p2 = p2.Or(p1, mask)
+		p1 = p2
+	}
+	return p1.BitLen() - 1
 }
