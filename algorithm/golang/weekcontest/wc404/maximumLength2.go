fc59b1315348be7a16f912710e038a8bd0a440cd
leiwingqueen <leiwingqueen@gmail.com>
2024-06-30 12:16:26 +0800

    404场周赛

--- algorithm/golang/weekcontest/wc404/maximumLength2.go
+++ algorithm/golang/weekcontest/wc404/maximumLength2.go
@@ -22,3 +22,33 @@ func maximumLength2(nums []int, k int) int {
 	}
 	return res
 }
+
+// 修复2
+func maximumLength3(nums []int, k int) int {
+	n := len(nums)
+	dp := make([][][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([][]int, k)
+		for j := 0; j < k; j++ {
+			dp[i][j] = make([]int, k)
+		}
+	}
+	dp[1][(nums[0]+nums[1])%k][nums[1]%k] = 2
+	for i := 2; i < n; i++ {
+		for j := 0; j < k; j++ {
+			for l := 0; l < k; l++ {
+				dp[i][j][l] = dp[i-1][j][l]
+				if nums[i]%k == l {
+					dp[i][j][l] = max(dp[i][j][l], dp[i-1][j][(j+k-l)%k]+1)
+				}
+			}
+		}
+	}
+	res := 0
+	for i := 0; i < k; i++ {
+		for j := 0; j < k; j++ {
+			res = max(res, dp[n-1][i][j])
+		}
+	}
+	return res
+}
--- algorithm/golang/weekcontest/wc404/maximumLength2_test.go
+++ algorithm/golang/weekcontest/wc404/maximumLength2_test.go
@@ -14,10 +14,11 @@ func Test_maximumLength2(t *testing.T) {
 	}{
 		// TODO: Add test cases.
 		{"t1", args{[]int{1, 2, 3, 4, 5}, 2}, 5},
+		{"t2", args{[]int{1, 2, 3, 10, 2}, 6}, 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := maximumLength2(tt.args.nums, tt.args.k); got != tt.want {
+			if got := maximumLength3(tt.args.nums, tt.args.k); got != tt.want {
 				t.Errorf("maximumLength2() = %v, want %v", got, tt.want)
 			}
 		})
