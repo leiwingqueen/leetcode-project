672c1d663c72f77b7248ae46d22be4e1610149e2
leiwingqueen <leiwingqueen@gmail.com>
2024-07-14 18:34:42 +0800

    周赛

--- algorithm/golang/weekcontest/wc406/minimumCost.go
+++ algorithm/golang/weekcontest/wc406/minimumCost.go
@@ -3,25 +3,34 @@ package wc406
 import "math"
 
 func minimumCost(m int, n int, horizontalCut []int, verticalCut []int) int {
+	cache := make(map[int]int)
+	buildKey := func(x1, x2, y1, y2 int) int {
+		return x1 | x2<<5 | y1<<10 | y2<<20
+	}
 	var dfs func(x1, y1, x2, y2 int) int
 	dfs = func(x1, y1, x2, y2 int) int {
 		if x2-x1 <= 1 && y2-y1 <= 1 {
 			return 0
 		}
+		key := buildKey(x1, x2, y1, y2)
+		if v, ok := cache[key]; ok {
+			return v
+		}
 		res := math.MaxInt
 		if x2-x1 > 1 {
-			for i := x1; i < x2; i++ {
+			for i := x1; i < x2-1; i++ {
 				sub := dfs(x1, y1, i+1, y2) + dfs(i+1, y1, x2, y2) + horizontalCut[i]
 				res = min(res, sub)
 			}
 		}
 		if y2-y1 > 1 {
-			for i := y1; i < y2; i++ {
+			for i := y1; i < y2-1; i++ {
 				sub := dfs(x1, y1, x2, i+1) + dfs(x1, i+1, x2, y2) + verticalCut[i]
 				res = min(res, sub)
 			}
 		}
+		cache[key] = res
 		return res
 	}
-	return dfs(0, 0, m-1, n-1)
+	return dfs(0, 0, m, n)
 }
