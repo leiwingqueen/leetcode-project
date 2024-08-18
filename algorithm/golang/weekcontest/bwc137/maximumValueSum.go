a001f4b65c86e2ac7aca1da3b0b7a5ca94be6989
leiwingqueen <leiwingqueen@gmail.com>
2024-08-18 12:19:38 +0800

    周赛

--- algorithm/golang/weekcontest/bwc137/maximumValueSum.go
+++ algorithm/golang/weekcontest/bwc137/maximumValueSum.go
@@ -1,6 +1,9 @@
 package bwc137
 
-import "math"
+import (
+	"math"
+	"sort"
+)
 
 func maximumValueSum(board [][]int) int64 {
 	m, n := len(board), len(board[0])
@@ -22,7 +25,7 @@ func maximumValueSum(board [][]int) int64 {
 	return dfs(m, n, 3)
 }
 
-// 增加记忆
+// 超时
 func maximumValueSum2(board [][]int) int64 {
 	m, n := len(board), len(board[0])
 	var dfs func(idx int) int64
@@ -59,4 +62,40 @@ func maximumValueSum2(board [][]int) int64 {
 		return res
 	}
 	return dfs(0)
+}
+
+// Store the largest 3 values for each row.
+// Select any 3 rows and brute force all combinations.
+func maximumValueSum3(board [][]int) int64 {
+	m := len(board)
+	matrix := make([][]int, m)
+	for i := 0; i < m; i++ {
+		matrix[i] = make([]int, 3)
+	}
+	for i := 0; i < m; i++ {
+		sort.Slice(board[i], func(k, j int) bool {
+			return board[i][k] > board[i][j]
+		})
+		for j := 0; j < 3; j++ {
+			matrix[i][j] = board[i][j]
+		}
+	}
+	var res int64
+	res = math.MinInt64
+	for x1 := 0; x1 < m; x1++ {
+		for y1 := 0; y1 < 3; y1++ {
+			for x2 := x1 + 1; x2 < m; x2++ {
+				for y2 := 0; y2 < 3; y2++ {
+					for x3 := x2 + 1; x3 < m; x3++ {
+						for y3 := 0; y3 < 3; y3++ {
+							if y1 != y2 && y1 != y3 && y2 != y3 {
+								res = max(res, int64(matrix[x1][y1])+int64(matrix[x2][y2])+int64(matrix[x3][y3]))
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return res
 }
