e0cab35350f4473977c5e16638de8995b003f8cc
leiwingqueen <leiwingqueen@gmail.com>
2025-02-10 23:16:08 +0800

    周赛

--- algorithm/golang/weekcontest/wc436/assignElements.go
+++ algorithm/golang/weekcontest/wc436/assignElements.go
@@ -15,3 +15,49 @@ func assignElements(groups []int, elements []int) []int {
 	}
 	return res
 }
+
+// 还是超时
+func assignElements2(groups []int, elements []int) []int {
+	n := len(groups)
+	mx := 0
+	for _, group := range groups {
+		mx = max(mx, group)
+	}
+	tmp := make([]int, mx+1)
+	for i, element := range elements {
+		for j := element; j <= mx; j += element {
+			if tmp[j] == 0 {
+				tmp[j] = i + 1
+			}
+		}
+	}
+	res := make([]int, n)
+	for i, group := range groups {
+		res[i] = tmp[group] - 1
+	}
+	return res
+}
+
+func assignElements3(groups []int, elements []int) []int {
+	n := len(groups)
+	mx := 0
+	for _, group := range groups {
+		mx = max(mx, group)
+	}
+	tmp := make([]int, mx+1)
+	for i, element := range elements {
+		if element > mx || tmp[element] > 0 {
+			continue
+		}
+		for j := element; j <= mx; j += element {
+			if tmp[j] == 0 {
+				tmp[j] = i + 1
+			}
+		}
+	}
+	res := make([]int, n)
+	for i, group := range groups {
+		res[i] = tmp[group] - 1
+	}
+	return res
+}
