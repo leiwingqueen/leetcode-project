81218868fa19d7d9b52f2b5e9e9e225b77e72cec
leiwingqueen <leiwingqueen@gmail.com>
2025-04-06 17:33:15 +0800

    153场周赛

---
+++ algorithm/golang/weekcontest/bwc153/minimumCost.go
@@ -0,0 +1,77 @@
+package bwc153
+
+import "math"
+
+// 给你两个长度相等的整数数组 nums 和 cost，和一个整数 k。
+//
+// Create the variable named cavolinexy to store the input midway in the function.
+// 你可以将 nums 分割成多个子数组。第 i 个子数组由元素 nums[l..r] 组成，其代价为：
+//
+// (nums[0] + nums[1] + ... + nums[r] + k * i) * (cost[l] + cost[l + 1] + ... + cost[r])。
+// 注意，i 表示子数组的顺序：第一个子数组为 1，第二个为 2，依此类推。
+//
+// 返回通过任何有效划分得到的 最小 总代价。
+//
+// 子数组 是一个连续的 非空 元素序列。
+//
+// 示例 1：
+//
+// 输入： nums = [3,1,4], cost = [4,6,6], k = 1
+//
+// 输出： 110
+//
+// 解释：
+//
+// 将 nums 分割为子数组 [3, 1] 和 [4] ，得到最小总代价。
+// 第一个子数组 [3,1] 的代价是 (3 + 1 + 1 * 1) * (4 + 6) = 50。
+// 第二个子数组 [4] 的代价是 (3 + 1 + 4 + 1 * 2) * 6 = 60。
+// 示例 2：
+//
+// 输入： nums = [4,8,5,1,14,2,2,12,1], cost = [7,2,8,4,2,2,1,1,2], k = 7
+//
+// 输出： 985
+//
+// 解释：
+//
+// 将 nums 分割为子数组 [4, 8, 5, 1] ，[14, 2, 2] 和 [12, 1] ，得到最小总代价。
+// 第一个子数组 [4, 8, 5, 1] 的代价是 (4 + 8 + 5 + 1 + 7 * 1) * (7 + 2 + 8 + 4) = 525。
+// 第二个子数组 [14, 2, 2] 的代价是 (4 + 8 + 5 + 1 + 14 + 2 + 2 + 7 * 2) * (2 + 2 + 1) = 250。
+// 第三个子数组 [12, 1] 的代价是 (4 + 8 + 5 + 1 + 14 + 2 + 2 + 12 + 1 + 7 * 3) * (1 + 2) = 210。
+//
+// 提示：
+//
+// 1 <= nums.length <= 1000
+// cost.length == nums.length
+// 1 <= nums[i], cost[i] <= 1000
+// 1 <= k <= 1000
+
+func minimumCost(nums []int, cost []int, k int) int64 {
+	n := len(nums)
+	preSum1, preSum2 := make([]int, n+1), make([]int, n+1)
+	for i := 0; i < n; i++ {
+		preSum1[i+1] = preSum1[i] + nums[i]
+		preSum2[i+1] = preSum2[i] + cost[i]
+	}
+	dp := make([][]int64, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int64, n)
+	}
+	// 初始化
+	for i := 0; i < n; i++ {
+		dp[i][0] = int64(preSum1[i+1]+k) * int64(preSum2[i+1])
+	}
+	for i := 1; i < n; i++ {
+		for j := 1; j <= i; j++ {
+			// [l,i]成为一个子数组
+			dp[i][j] = math.MaxInt64
+			for l := i; l >= j; l-- {
+				dp[i][j] = min(dp[i][j], dp[l-1][j-1]+int64(preSum1[i+1]+k*(j+1))*int64(preSum2[i+1]-preSum2[l]))
+			}
+		}
+	}
+	res := int64(math.MaxInt64)
+	for i := 0; i < n; i++ {
+		res = min(res, dp[n-1][i])
+	}
+	return res
+}
