37ab51c9b966db8559d27582b192f1c76c4de0fd
leiwingqueen <leiwingqueen@gmail.com>
2025-08-17 16:57:48 +0800

    463场周赛

---
+++ algorithm/golang/weekcontest/wc463/maxProfit_test.go
@@ -0,0 +1,26 @@
+package wc463
+
+import "testing"
+
+func Test_maxProfit(t *testing.T) {
+	type args struct {
+		prices   []int
+		strategy []int
+		k        int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+		{"t1", args{[]int{5, 4, 3}, []int{1, 1, 0}, 2}, 9},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxProfit(tt.args.prices, tt.args.strategy, tt.args.k); got != tt.want {
+				t.Errorf("maxProfit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
