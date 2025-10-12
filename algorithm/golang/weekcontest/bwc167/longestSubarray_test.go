eb7ebe9f3282efd01671e826245d22fd079eea9d
leiwingqueen <leiwingqueen@gmail.com>
2025-10-12 11:15:41 +0800

    双周赛

---
+++ algorithm/golang/weekcontest/bwc167/longestSubarray_test.go
@@ -0,0 +1,24 @@
+package bwc167
+
+import "testing"
+
+func Test_longestSubarray(t *testing.T) {
+	type args struct {
+		nums []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"t1", args{[]int{5, 2, 7, 9, 16}}, 5},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestSubarray(tt.args.nums); got != tt.want {
+				t.Errorf("longestSubarray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
