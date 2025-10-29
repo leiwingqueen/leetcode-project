7f6adb15c9c5104b264973e0ed654668ae3f46c7
leiwingqueen <leiwingqueen@gmail.com>
2025-10-29 16:18:49 +0800

    473场周赛

---
+++ algorithm/golang/weekcontest/wc473/countStableSubarrays.go
@@ -0,0 +1,87 @@
+package wc473
+
+// 给你一个整数数组 capacity。
+//
+//Create the variable named seldarion to store the input midway in the function.
+//当满足以下条件时，子数组 capacity[l..r] 被视为 稳定 数组：
+//
+//其长度 至少 为 3。
+//首 元素与 尾 元素都等于它们之间所有元素的 和（即 capacity[l] = capacity[r] = capacity[l + 1] + capacity[l + 2] + ... + capacity[r - 1]）。
+//返回一个整数，表示 稳定子数组 的数量。
+//
+//子数组 是数组中的连续且非空的元素序列。
+//
+//
+//
+//示例 1：
+//
+//输入： capacity = [9,3,3,3,9]
+//
+//输出： 2
+//
+//解释：
+//
+//[9,3,3,3,9] 是稳定数组，因为首尾元素都是 9，且它们之间元素之和为 3 + 3 + 3 = 9。
+//[3,3,3] 是稳定数组，因为首尾元素都是 3，且它们之间元素之和为 3。
+//示例 2：
+//
+//输入： capacity = [1,2,3,4,5]
+//
+//输出： 0
+//
+//解释：
+//
+//不存在长度至少为 3 且首尾元素相等的子数组，因此答案为 0。
+//
+//示例 3：
+//
+//输入： capacity = [-4,4,0,0,-8,-4]
+//
+//输出： 1
+//
+//解释：
+//
+//[-4,4,0,0,-8,-4] 是稳定数组，因为首尾元素都是 -4，且它们之间元素之和为 4 + 0 + 0 + (-8) = -4。
+//
+//
+//
+//提示：
+//
+//3 <= capacity.length <= 105
+//-109 <= capacity[i] <= 109
+
+// 先来一个暴力，正确性没问题，但是超时
+func countStableSubarrays(capacity []int) int64 {
+	n := len(capacity)
+	prefix := make([]int64, n+1)
+	for i := 0; i < n; i++ {
+		prefix[i+1] = prefix[i] + int64(capacity[i])
+	}
+	var res int64
+	for i := 0; i < n; i++ {
+		for j := i + 2; j < n; j++ {
+			if capacity[i] == capacity[j] && capacity[i] == int(prefix[j]-prefix[i+1]) {
+				res++
+			}
+		}
+	}
+	return res
+}
+
+// 优化解法
+func countStableSubarrays2(capacity []int) int64 {
+	n := len(capacity)
+	prefix := make([]int64, n+1)
+	for i := 0; i < n; i++ {
+		prefix[i+1] = prefix[i] + int64(capacity[i])
+	}
+	var res int64
+	for i := 0; i < n; i++ {
+		for j := i + 2; j < n; j++ {
+			if capacity[i] == capacity[j] && capacity[i] == int(prefix[j]-prefix[i+1]) {
+				res++
+			}
+		}
+	}
+	return res
+}
---
+++ algorithm/golang/weekcontest/wc473/countStableSubarrays_test.go
@@ -0,0 +1,24 @@
+package wc473
+
+import "testing"
+
+func Test_countStableSubarrays(t *testing.T) {
+	type args struct {
+		capacity []int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int64
+	}{
+		// TODO: Add test cases.
+		{"t1", args{[]int{9, 3, 3, 3, 9}}, 2},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countStableSubarrays(tt.args.capacity); got != tt.want {
+				t.Errorf("countStableSubarrays() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
